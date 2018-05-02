05f2c319e5e90f89b724546a015a3da2d2e8db65
tiantang88 <466684860@qq.com>
2018-05-02 13:46:31 +0800

    订单model提交

---
+++ app/Order/Models/OrderGoods.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ *  订单设备列表
+ */
+namespace App\Order\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderGood extends Model
+{
+
+    protected $table = 'order_goods';
+
+    protected $primaryKey='id';
+
+
+
+
+}
