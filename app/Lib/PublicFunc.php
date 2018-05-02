dbdf69a5139524962f1dcfadeb0de3fcba91ef5d
maxiaoyu <maxiaoyu@huishoubao.com>
2018-05-02 16:57:31 +0800

    打印函数

--- app/Lib/PublicFunc.php
+++ app/Lib/PublicFunc.php
@@ -17,3 +17,28 @@ function apiResponse($data=[], $errno=0, $errmsg='')
 {
     return response()->json(['data'=>$data, 'code'=>$errno, 'msg'=>$errmsg]);
 }
+
+/**
+ * 打印函数 print_r
+ * @param $data 打印数组
+ */
+function p($data, $exit = '')
+{
+    echo "<pre>";
+    print_r($data);
+    if($exit != ""){
+        exit;
+    }
+}
+/**
+ * 打印函数 var_dump
+ * @param $data 打印数组
+ */
+function v($data, $exit = '')
+{
+    echo "<pre>";
+    var_dump($data);
+    if($exit != ""){
+        exit;
+    }
+}
