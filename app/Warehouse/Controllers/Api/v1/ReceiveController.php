d696a01b3e16ebbbcf41b47b9edf898d87b8a888
wangjinlin <285253001@qq.com>
2018-05-07 18:25:06 +0800

    发货单

--- app/Warehouse/Controllers/Api/v1/ReceiveController.php
+++ app/Warehouse/Controllers/Api/v1/ReceiveController.php
@@ -2,18 +2,18 @@
 
 namespace App\Warehouse\Controllers\Api\v1;
 use App\Lib\ApiStatus;
-use App\Order\Modules\Service;
+use App\Warehouse\Modules\Service;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ReceiveController extends Controller
 {
-    protected $OrderCreate;
+    protected $DeliveryCreate;
 
-    public function __construct(Service\OrderCreater $OrderCreate)
+    public function __construct(Service\DeliveryCreater $DeliveryCreate)
     {
-        $this->OrderCreate = $OrderCreate;
+        $this->DeliveryCreate = $DeliveryCreate;
     }
 
 
