f397d2c563e83dac2f925c2e42ca3ca3186da055
wangjinlin <285253001@qq.com>
2018-05-09 19:15:30 +0800

    mod

---
+++ app/Warehouse/Models/Warehouse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Warehouse\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Warehouse extends Model
+{
+    /**
+     * 设置当前数据库
+     */
+    public function __construct() {
+        $this->setConnection('warehouse');
+    }
+
+
+}
