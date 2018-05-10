0a0d281ded91c62284b5dbb8873b4c39aff4d02c
qinliping <qinliping@huishoubao.com.cn>
2018-05-10 19:57:35 +0800

    申请退货

--- app/Order/Modules/Service/OrderReturnCreater.php
+++ app/Order/Modules/Service/OrderReturnCreater.php
@@ -1,39 +1,36 @@
 <?php
 namespace App\Order\Modules\Service;
 use App\Lib\ApiStatus;
+use App\Lib\OldInc;
 use \App\Lib\Common\SmsApi;
 use Illuminate\Support\Facades\DB;
 use App\Order\Modules\Repository\OrderReturnRepository;
 use App\Order\Modules\Repository\OrderRepository;
+use App\Order\Modules\Repository\ThirdInterface;
+use App\Order\Modules\Repository\OrderGoodsRepository;
 
 class OrderReturnCreater
 {
 
     protected $orderReturnRepository;
     protected $orderRepository;
-    public function __construct(orderReturnRepository $orderReturnRepository,orderRepository $orderRepository)
+    protected $orderGoodsRepository;
+    public function __construct(orderReturnRepository $orderReturnRepository,orderRepository $orderRepository,orderGoodsRepository $orderGoodsRepository)
     {
-        
+
         $this->orderReturnRepository = $orderReturnRepository;
         $this->orderRepository = $orderRepository;
-        
+        $this->orderGoodsRepository =$orderGoodsRepository;
     }
     public function get_return_info($data){
         return $this->orderReturnRepository->get_return_info($data);
     }
     //添加退换货数据
     public function add($data){
-        $to_data['order_no']=$data['order_no'];
-        $to_data['goods_no']=$data['goods_no'];
-        $to_data['user_id']=$data['user_id'];
-        $to_data['business_key']=$data['business_key'];
-        $to_data['loss_type']=$data['loss_type'];
-        $to_data['reason_id']=$data['reason_id'];
-        $to_data['reason_text']=$data['reason_text'];
-        $to_data['status']='1';
-        $to_data['refund_no']=createNo('2');
-        $to_data['create_time']=time();
-        return $this->orderReturnRepository->add($to_data);
+        $data['status']='1';
+        $data['refund_no']=createNo('2');
+        $data['create_time']=time();
+        return $this->orderReturnRepository->add($data);
     }
     /**
      * 管理员审核 --同意
@@ -68,12 +65,25 @@ class OrderReturnCreater
             return apiResponse([],ApiStatus::CODE_33004,ApiStatus::$errCodes[ApiStatus::CODE_33004]);
         }
         $res= $this->orderReturnRepository->update_return($params);
-        if($res){
-                if($this->orderReturnRepository->goods_update($params['order_no'],$params['status'])) {
-                    //申请退货同意发送短信
 
-                    return ApiStatus::CODE_0;//成功
-                }
+        if($res){
+            if($this->orderReturnRepository->goods_update($params['order_no'],$params['status'])) {
+                //获取订单信息
+                $data['orderNo']=$params['order_no'];
+                $order_info= $this->orderRepository->getOrderInfo($data);
+                //获取商品信息
+                $goods_info= $this->orderReturnRepository->get_goods_info($params['order_no']);
+                //申请退货同意发送短信
+                $b =SmsApi::sendMessage($order_info[0]->mobile,'SMS_113455999',[
+                    'realName' =>$order_info[0]->realName,
+                    'orderNo' => $order_info[0]->order_no,
+                    'goodsName' => $goods_info[0]['good_name'],
+                    'shoujianrenName' => "test",
+                    'returnAddress' => "test",
+                    'serviceTel'=>OldInc::Customer_Service_Phone,
+                ],$order_info[0]->order_no);
+                return ApiStatus::CODE_0;//成功
+            }
         }else{
             return ApiStatus::CODE_33008;//更新审核状态失败
         }
@@ -108,13 +118,209 @@ class OrderReturnCreater
         $res = $this->orderReturnRepository->deny_return($params);
         if($res){
             if($this->orderRepository->deny_update($params['order_no'])){
+                //获取订单信息
+                $data['orderNo']=$params['order_no'];
+                $order_info= $this->orderRepository->getOrderInfo($data);
+                //获取商品信息
+                $goods_info= $this->orderReturnRepository->get_goods_info($params['order_no']);
+
+                if(!$goods_info){
+                    return ApiStatus::CODE_40000;//商品信息错误
+                }
                 //申请退货拒绝发送短信
+                // SmsApi::sendMessage($user_info['mobile'],1,array());
+                $b = SmsApi::sendMessage('13020059043','hsb_sms_d284d',[
+                    'realName' =>$order_info[0]->realname,
+                    'orderNo' =>$order_info[0]->order_no,
+                    'goodsName' => $goods_info[0]['good_name'],
+                    'serviceTel'=>OldInc::Customer_Service_Phone,
+                ],$order_info[0]->order_no);
                 return ApiStatus::CODE_0;//成功
             }else{
                 return ApiStatus::CODE_33007;//更新审核状态失败
             }
         }else{
             return ApiStatus::CODE_33008;//更新审核状态失败
+        }
+
+    }
+    //取消退货申请
+    public function cancel_apply($params){
+        $res = $this->orderReturnRepository->cancel_apply($params);
+        if($res){
+            $order = $this->orderRepository->deny_update($params['order_no']);
+            if($order){
+                //修改商品状态
+                $order_goods = $this->orderReturnRepository->cancel_goods_update($params['order_no']);
+                if($order_goods){
+                    return ApiStatus::CODE_0;//成功
+                }else{
+                    return ApiStatus::CODE_33009;//const CODE_33008 = '33008'; //[退换货]修改退换货状态失败
+                }
+            }else{
+                return ApiStatus::CODE_33007;//更新订单状态失败
+            }
+
+        }else{
+            return ApiStatus::CODE_33008;//更新审核状态失败
+        }
+    }
+    //获取退款单信息
+    public function get_info_by_order_no($order_no){
+        if(empty($order_no)){
+            return ApiStatus::CODE_33003;//const CODE_33008 = '33008'; //[退换货]订单编号不能为空
+        }
+        return $this->orderReturnRepository->get_info_by_order_no($order_no);
+    }
+    //上传物流单号
+    public function upload_wuliu($data){
+        return $this->orderReturnRepository->upload_wuliu($data);
+    }
+    //获取退换货订单列表方法
+    public function get_list($params)
+    {
+
+        $page = empty($params['page']) ? 1 : $params['page'];
+        $size = !empty($params['size']) ? $params['size'] : config('web.pre_page_size');
+        $where = [];
+
+        if (isset($params['begin_time'])!= '') {
+          $where['begin_time'] = strtotime($params['begin_time']);
+        }
+        if (isset($params['end_time'] )!= '') {
+            $where['end_time'] = strtotime($params['end_time']);
+         }
+
+        if (isset($params['business_key']) > 0) {
+            $where['business_key'] = intval($params['business_key']);
+        }
+        if (isset($params['keywords']) != '') {
+            if (isset($params['kw_type']) == 'goods_name') {
+                $where['goods_name'] = $params['keywords'];
+            } elseif (isset($params['kw_type']) == 'order_no') {
+                $where['order_no'] = $params['keywords'];
+            } elseif (isset($params['kw_type']) == 'user_id') {
+                $user_info = ThirdInterface::GetUser($params['keywords']);
+                if (empty($user_info)) {
+                    // 如果没有用户  直接返回空
+                    return apiResponse([], ApiStatus::CODE_0, 'success');
+                } else {
+                    $where['user_id'] = $user_info['user_id'];
+                }
+
+            }
+
+        }
+        if (isset($params['return_status']) && $params['return_status'] > 0) {
+            $where['status'] = intval($params['return_status']);
+        }
+        if (isset($params['user_id'])!='') {
+            $where['user_id'] = $params['user_id'];
+        }
+        // 查询退货申请单
+        $additional['page'] = $page;
+        $additional['limit'] = $size;
+        $where = $this->_parse_order_where($where);
+        $data = $this->orderReturnRepository->get_list($where, $additional);
+        return $data;
+    }
+
+/** 查询条件过滤
+ * @param array $where	【可选】查询条件
+ * [
+ *      'return_id' => '',	//【可选】mixed 退货申请单ID，string|array （string：多个','分割）（array：ID数组）多个只支持
+ *      'case_id' => '',	//【可选】string；退货原因ID
+ *      'status'=>''      //【可选】int；阶段
+ *      'begin_time'=>''      //【可选】int；开始时间戳
+ *      'end_time'=>''      //【可选】int；  截止时间戳
+ * ]
+ * @return array	查询条件
+ */
+public function _parse_order_where($where=[]){
+
+      $where = filter_array($where, [
+          'business_key' => 'required|is_id',
+          'goods_name' => 'required',
+          'order_no' => 'required|is_string',
+          'status' => 'required|is_int',
+          'begin_time'  => 'required',
+          'end_time'    => 'required',
+          'user_id' => 'required|is_id',
+      ]);
+      // 结束时间（可选），默认为为当前时间
+      if( !isset($where['end_time']) ){
+          $where['end_time'] = time();
+      }
+      if( isset($where['user_id']) ){
+          $where1[] = ['order_return.user_id', '=', $where['user_id']];
+      }
+      // 开始时间（可选）
+      if( isset($where['begin_time'])){
+        if( $where['begin_time']>$where['end_time'] ){
+            return false;
+        }
+        $where1[] = ['order_return.create_time', '>=', $where['begin_time']];
+        $where1[] = ['order_return.create_time', '<=', $where['end_time']];
+      }else{
+          $where1[] = ['order_return.create_time', '<', $where['end_time']];
+      }
+      unset($where['begin_time']);
+      unset($where['end_time']);
+    // order_no 订单编号查询，使用前缀模糊查询
+    if( isset($where['order_no']) ){
+        $where1[] = ['order_return.order_no', 'like', $where['order_no'].'%'];
+    }
+    // order_no 订单编号查询，使用前缀模糊查询
+    if( isset($where['goods_name'])){
+        $where1[] = ['order_goods.goods_name', 'like', $where['goods_name'].'%'];
+    }
+    if( isset($where['status']) ){
+        $where1[] = ['order_return.status', '=', $where['status']];
+    }
+    if( isset($where['business_key']) ){
+        $where1[] = ['order_return.business_key', '=', $where['business_key']];
+    }
+    return $where1;
+}
+    //获取商品信息
+    public function get_goods_info($goods_no){
+        if(empty($goods_no)){
+            return ApiStatus::CODE_20001;//商品编号不能为空
+        }
+        return $this->orderGoodsRepository->getgoodsList($goods_no);
+
+    }
+    //退货结果查看
+    public function returnResult($params){
+        if(empty($params['order_no'])){
+            return apiResponse( [], ApiStatus::CODE_33003,'订单编号不能为空' );
+        }
+        if(empty($params['goods_no'])){
+            return apiResponse( [], ApiStatus::CODE_40000,'商品编号不能为空' );
+        }
+       $result= $this->orderReturnRepository->returnResult($params);
+        if($result){
+            return $result;
+        }else{
+            return false;
+        }
+
+    }
+    //检测合格
+    public function is_qualified($params){
+        if(empty($params['goods_no'])){
+            return ApiStatus::CODE_40000;//商品信息错误
+        }
+        if(empty($params['order_no'])){
+            return ApiStatus::CODE_33003;//订单编号不能为空
+        }
+        if(empty($params['status'])){
+            return ApiStatus::CODE_34004;//退货状态不能为空
+        }
+        $result= $this->orderReturnRepository->is_qualified($params);
+        if($result){
+            $data['deposit_unfreeze_amount']=$result['price']-$result['insurance'];//退还押金金额=实际支付金额-意外险
+            //创建记录
         }
 
     }
