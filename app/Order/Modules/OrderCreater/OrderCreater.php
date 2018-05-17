3a5bef72aedf9853cba4a99051aa86190cb7cd47
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-05-17 15:30:23 +0800

    增加组件

---
+++ app/Order/Modules/OrderCreater/OrderCreater.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 下单接口
+ * @access public (访问修饰符)
+ * @author wuhaiyan <wuhaiyan@huishoubao.com>
+ * @copyright (c) 2017, Huishoubao
+ */
+
+namespace App\Order\Modules\OrderCreater;
+
+
+interface OrderCreater
+{
+    /**
+     * 获取订单创建器
+     * @return OrderCreater
+     */
+    public function getOrderCreater():OrderCreater;
+    /**
+     * 过滤
+     * <p>注意：</p>
+     * <p>在过滤过程中，可以修改下单需要的元数据</p>
+     * <p>组件之间的过滤操作互不影响</p>
+     * <p>先执行内部组件的filter()，然后再执行组件本身的过滤</p>
+     * @return bool
+     */
+    public function filter(): bool;
+
+    /**
+     * 获取数据结构
+     * @return array
+     */
+    public function getDataSchema(): array;
+
+    /**
+     * 创建数据
+     * @return bool
+     */
+    public function create(): bool;
+
+
+
+}
