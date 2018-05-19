3c76cdcf23416a4e97b4c6feb1080d1cf5e7906c
yaodongxu <yaodongxu@huishoubao.com.cn>
2018-05-19 16:38:49 +0800

    增加还机单生成编号

--- app/Lib/PublicFunc.php
+++ app/Lib/PublicFunc.php
@@ -231,7 +231,7 @@ function v($data, $exit = '')
 /**
  * 规则：前缀（1位）+年（1位）+月（1位）+日（2位）+时间戳（5位）+微秒（5位）+随机数（1位）
  * Author: heaven
- * @param int $noType   1分期交易号, 2退货编号, 3支付交易, 4预授权 5,业务平台退款码6.goods_no生成方式
+ * @param int $noType   1分期交易号, 2退货编号, 3支付交易, 4预授权 5,业务平台退款码6.goods_no生成方式,7.还机单编号
  * @return bool|string
 
  */
@@ -244,6 +244,7 @@ function createNo($noType=1){
         4 => 'Y',
         5 => 'C',
         6 => 'G',
+        7 => 'H',
     );
     $year = array();
     if (!isset($npreNoType[$noType])) {
