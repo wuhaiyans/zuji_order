53c0c43cb2b4fb8c835f7f9246849c7ca622bede
wansq <cboy868@163.com>
2018-05-21 19:43:14 +0800

    delivery 更新

--- app/Warehouse/Models/Delivery.php
+++ app/Warehouse/Models/Delivery.php
@@ -25,7 +25,7 @@ class Delivery extends Warehouse
 
     protected $primaryKey='delivery_no';
 
-    protected $fillable = ['delivery_no', 'order_no', 'logistics_id','status_time','app_id','customer',
+    protected $fillable = ['delivery_no', 'order_no', 'logistics_id','status_time','app_id','customer','is_auto',
         'customer_mobile','customer_address','logistics_no', 'status', 'create_time', 'delivery_time', 'status_remark'];
 
 
