c9f7b5dac618924a87fbde963e63a8565ed66c56
liuhongxing <1342935834@qq.com>
2018-05-22 02:45:44 +0800

    更新：打印 代扣解约同步结果

--- app/Order/Controllers/Api/v1/PayController.php
+++ app/Order/Controllers/Api/v1/PayController.php
@@ -51,11 +51,12 @@ class PayController extends Controller
 	// 测试 代扣解约
 	public function testWithholdUnsign(){
 		
-		\App\Lib\Payment\CommonWithholdingApi::unSign([
+		$info = \App\Lib\Payment\CommonWithholdingApi::unSign([
     		'user_id'		=> '5', //租机平台用户ID
     		'agreement_no'	=> '30A51306522705601', //支付平台签约协议号
     		'back_url'		=> env('APP_URL').'/order/pay/withholdUnsignNotify', //后端回调地址
 		]);
+		var_dump( $info );exit;
 	}
 		
 	
