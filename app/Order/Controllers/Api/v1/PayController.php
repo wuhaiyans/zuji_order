9c4f8c086bea41f1557374addf6d84c26529bc37
liuhongxing <1342935834@qq.com>
2018-05-26 01:47:31 +0800

    更新

--- app/Order/Controllers/Api/v1/PayController.php
+++ app/Order/Controllers/Api/v1/PayController.php
@@ -112,7 +112,7 @@ class PayController extends Controller
 		
 		
 		$business_type = 1; 
-		$business_no = 'FA52283402709388';
+		$business_no = 'FA52283402709389';
 		$pay = null;
 		try {
 			// 查询
