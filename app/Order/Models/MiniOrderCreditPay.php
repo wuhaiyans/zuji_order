c48fd5b776fa6b26fc448d065ded112967f82298
zhangjinhui <zhangjinhui>
2018-05-28 20:27:02 +0800

    小程序

---
+++ app/Order/Models/MiniOrderCreditPay.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *
+ *  小程序发送 扣款 取消 完成请求信息记录表
+ */
+namespace App\Order\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MiniOrderCreditPay extends Model
+{
+
+    protected $table = 'mini_order_credit_pay';
+
+    protected $primaryKey='id';
+
+
+    /**
+     * 默认使用时间戳戳功能
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * 可以被批量赋值的属性.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_operate_type',
+        'out_order_no',
+        'zm_order_no',
+        'out_trans_no',
+        'remark',
+        'pay_amount',
+    ];
+
+
+}
---
+++ app/Order/Models/MiniOrderNotifyLog.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ *
+ *  小程序 请求回调 记录表
+ */
+namespace App\Order\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MiniOrderNotifyLog extends Model
+{
+
+    protected $table = 'mini_order_notify_log';
+
+    protected $primaryKey='id';
+
+
+    /**
+     * 默认使用时间戳戳功能
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * 可以被批量赋值的属性.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'notify_type',
+        'redis_key',
+        'out_order_no',
+        'zm_order_no',
+        'out_trans_no',
+        'alipay_fund_order_no',
+        'pay_time',
+        'pay_status',
+        'pay_amount',
+        'channel',
+        'notify_app_id',
+        'data_text',
+    ];
+
+
+}
