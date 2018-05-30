5524fd6a6ade34d1a3d89de4cad6252b8c60195f
yaodongxu <yaodongxu@huishoubao.com.cn>
2018-05-30 20:57:29 +0800

    打印订单商品服务层对象

--- app/Order/Modules/Service/OrderGiveback.php
+++ app/Order/Modules/Service/OrderGiveback.php
@@ -290,6 +290,7 @@ class OrderGiveback
 					return false;
 				}
 			}
+			var_dump($orderGoodsService);exit;
 			$orderGoodsResult = $orderGoodsService->update(['goods_no'=>$orderGivevbackInfo['goods_no']], ['status'=> $status]);
 				var_dump($orderGoodsResult);exit;
 			if( !$orderGoodsResult ){
