6c9758a53f1a0a8a0ce93210aaead85dc643fb64
tiantang88 <466684860@qq.com>
2018-05-31 15:03:13 +0800

    清算预授权接口联调

--- app/Order/Controllers/Api/v1/OrderCleaningController.php
+++ app/Order/Controllers/Api/v1/OrderCleaningController.php
@@ -6,6 +6,7 @@
  */
 namespace App\Order\Controllers\Api\v1;
 use App\Lib\ApiStatus;
+use App\Order\Modules\Repository\Pay\Channel;
 use App\Order\Modules\Repository\Pay\PayCreater;
 use Illuminate\Http\Request;
 use App\Order\Modules\Service\OrderCleaning;
@@ -187,70 +188,51 @@ class OrderCleaningController extends Controller
     {
 
         $business_type = 1;
-        $business_no = 'A528197713276854'; //\createNo(1);
+        $business_no = 'TA53135870325557';
         $pay = null;
         try {
             // 查询
             $pay = \App\Order\Modules\Repository\Pay\PayQuery::getPayByBusiness($business_type, $business_no);
-
             // 取消
-            //$pay->cancel();
+            $pay->cancel();
             // 恢复
-            //$pay->resume();
+            $pay->resume();
 
         } catch (\App\Lib\NotFoundException $exc) {
 
             // 创建支付
-            $pay = PayCreater::createPaymentWithholdFundauth([
+            $pay = \App\Order\Modules\Repository\Pay\PayCreater::createPayment([
                 'user_id'		=> '5',
                 'businessType'	=> $business_type,
                 'businessNo'	=> $business_no,
 
-                'paymentNo' => \createNo(1),
                 'paymentAmount' => '0.01',
-                'paymentChannel'=> Channel::Alipay,
+                'paymentChannel'=> \App\Order\Modules\Repository\Pay\Channel::Jdpay,
                 'paymentFenqi'	=> 0,
 
-                'withholdNo' => \createNo(1),
-                'withholdChannel'=> Channel::Alipay,
+                'withholdChannel'=> \App\Order\Modules\Repository\Pay\Channel::Alipay,
 
-                'fundauthNo' => \createNo(1),
                 'fundauthAmount' => '1.00',
-                'fundauthChannel'=> Channel::Alipay,
+                'fundauthChannel'=> \App\Order\Modules\Repository\Pay\Channel::Alipay,
             ]);
         } catch (\Exception $exc) {
             exit('error');
         }
 
-//        // 支付阶段状态
-//        $this->assertFalse( $pay->isSuccess(), '支付阶段状态错误' );
-//
-//
-//        // 支付状态
-//        $this->assertTrue( $pay->getPaymentStatus() == PaymentStatus::WAIT_PAYMENT,
-//            '支付环节状态初始化错误' );
-//
-//        // 代扣签约状态
-//        $this->assertTrue(  $pay->getWithholdStatus() == WithholdStatus::WAIT_WITHHOLD,
-//            '代扣签约状态初始化错误' );
-//
-//        // 资金预授权状态
-//        $this->assertTrue( $pay->getFundauthStatus() == FundauthStatus::WAIT_FUNDAUTH,
-//            '资金预授权状态初始化错误' );
-
         try {
             $step = $pay->getCurrentStep();
-            echo '当前阶段：'.$step."\n";
+            // echo '当前阶段：'.$step."\n";
 
             $_params = [
                 'name'			=> '测试支付',					//【必选】string 交易名称
-                'back_url'		=> 'https://alipay/Test/back',	//【必选】string 后台通知地址
-                'front_url'		=> 'https://alipay/Test/front',	//【必选】string 前端回跳地址
+                'front_url'		=> env('APP_URL').'/order/pay/testPaymentFront',	//【必选】string 前端回跳地址
             ];
-            $url_info = $pay->getCurrentUrl( 2,$_params );
-            p($url_info['url']);
-            header("Location: {$url_info['url']}");
-//            return redirect($url_info['url']);
+
+            $pay->setPaymentAmount(0.02);
+
+            $url_info = $pay->getCurrentUrl( \App\Order\Modules\Repository\Pay\Channel::Alipay, $_params );
+            header( 'Location: '.$url_info['url'] );
+//			var_dump( $url_info );
 
         } catch (\Exception $exc) {
             echo $exc->getMessage()."\n";
