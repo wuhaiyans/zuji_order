a6e034eb603e0e024baa39d785ccef65df3947aa
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-06-01 15:37:36 +0800

    代扣解约

--- app/Order/Controllers/Api/v1/WithholdController.php
+++ app/Order/Controllers/Api/v1/WithholdController.php
@@ -123,9 +123,9 @@ class WithholdController extends Controller
 
         try {
             $data = [
-                'user_id'           => $userId, //租机平台用户ID
-                'agreement_no'      => $withholdInfo['out_withhold_no'], //支付平台签约协议号
-                'out_agreement_no'  => $withholdInfo['withhold_no'],    //业务平台签约协议号
+                'user_id'           => $userId, //租机平台用户IDwithhold_no
+                'agreement_no'      => $withholdInfo['withhold_no'], //支付平台签约协议号
+                'out_agreement_no'  => $withholdInfo['out_withhold_no'],    //业务平台签约协议号
                 'back_url'          => env("API_INNER_URL") . "/unSignNotify", //回调地址
             ];
 
