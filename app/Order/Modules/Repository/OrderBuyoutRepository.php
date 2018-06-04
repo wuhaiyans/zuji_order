e588ca9bf1eacfc1795f1a27f64e6f21d7581c63
limin <641260770@qq.com>
2018-06-04 17:03:02 +0800

    保存

--- app/Order/Modules/Repository/OrderBuyoutRepository.php
+++ app/Order/Modules/Repository/OrderBuyoutRepository.php
@@ -44,7 +44,6 @@ class OrderBuyoutRepository
 				->leftJoin('order_info','order_buyout.order_no', '=', 'order_info.order_no')
 				->leftJoin('order_goods',[['order_buyout.order_no', '=', 'order_goods.order_no'],['order_buyout.goods_no', '=', 'order_goods.goods_no']])
 				->where($where)
-				->select('order_buyout.*','order_userinfo.*','order_info.*','order_goods.*')
 				->count();
 		return $count;
 	}
