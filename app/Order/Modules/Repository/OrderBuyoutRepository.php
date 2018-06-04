0f83e07e84ad60606cd6f91a92fe74809b077a90
limin <641260770@qq.com>
2018-06-04 19:57:53 +0800

    保存

--- app/Order/Modules/Repository/OrderBuyoutRepository.php
+++ app/Order/Modules/Repository/OrderBuyoutRepository.php
@@ -63,14 +63,13 @@ class OrderBuyoutRepository
 			return false;
 		}
 		$additional['offset'] = $additional['offset']* $additional['limit'];
-		$parcels = OrderBuyout::
-				//->leftJoin('order_userinfo', 'order_buyout.order_no', '=', 'order_userinfo.order_no')
-				//->leftJoin('order_info','order_buyout.order_no', '=', 'order_info.order_no')
-				//->leftJoin('order_goods',[['order_buyout.order_no', '=', 'order_goods.order_no'],['order_buyout.goods_no', '=', 'order_goods.goods_no']])
-				//->where($where)
-				get()->skip($additional['offset'])->take($additional['limit']);
-				//->select('order_buyout.*','order_userinfo.*','order_info.*','order_goods.*')
-		var_dump($parcels);die;
+		$parcels = OrderBuyout::query()
+				->leftJoin('order_userinfo', 'order_buyout.order_no', '=', 'order_userinfo.order_no')
+				->leftJoin('order_info','order_buyout.order_no', '=', 'order_info.order_no')
+				->leftJoin('order_goods',[['order_buyout.order_no', '=', 'order_goods.order_no'],['order_buyout.goods_no', '=', 'order_goods.goods_no']])
+				->where($where)
+				->select('order_buyout.*','order_userinfo.*','order_info.*','order_goods.*')
+				->paginate($additional['limit'],$columns = ['*'], $pageName = '', $additional['offset']);
 		if($parcels){
 			return $parcels->toArray();
 		}
