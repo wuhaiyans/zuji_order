accb36878de0c05f49148b5e63cdfdf06558a109
zhangjinhui <zhangjinhui>
2018-06-06 17:20:33 +0800

    打印

--- app/Lib/AlipaySdk/sdk/CommonMiniApi.php
+++ app/Lib/AlipaySdk/sdk/CommonMiniApi.php
@@ -106,7 +106,7 @@ class CommonMiniApi extends BaseApi {
 		$request = new \ZhimaMerchantOrderConfirmRequest();
 		$request->setBizContent(json_encode($biz_content));
 		$result = $this->execute ( $request);
-		print_r($result);
+		var_dump($result);
 		$debug_data = [
 			'request' => $biz_content,
 			'response' => json_decode(json_encode($result),true),
