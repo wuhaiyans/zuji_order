f2f0368e3ca58c111bc0374548f5b640db842548
liuhongxing <liuhongxing@huishoubao.com.cn>
2018-06-06 11:17:41 +0800

    提交

--- app/Order/Modules/Repository/Order/Creater/Component/SkuComponent.php
+++ app/Order/Modules/Repository/Order/Creater/Component/SkuComponent.php
@@ -6,10 +6,11 @@
  * @copyright (c) 2018, Huishoubao
  */
 
-namespace App\Order\Modules\Repository\Order\Creater;
+namespace App\Order\Modules\Repository\Order\Creater\Component;
 
+use App\Order\Modules\Repository\Order\Creater\OrderCreaterComponent;
 
-class SkuComponnet extends OrderCreaterComponnet
+class SkuComponent extends OrderCreaterComponent
 {
 
     /**
@@ -25,39 +26,19 @@ class SkuComponnet extends OrderCreaterComponnet
 	private $sku_info_list = [];
 	
 
-	/**
-	 * 构造函数
-	 * @param \App\Order\Modules\Repository\OrderCreater\OrderCreaterInterface $componnet
-	 * @param	array $sku_list	sku列表参数 
-	 * [
-	 *		[
-	 *			'sku_id'	=> '',	//【必须】int SKU ID
-	 *			'num'		=> '',	//【必须】int 数量
-	 *		]
-	 * ]
-	 * @throws \Exception
-	 */
-    public function __construct( OrderCreaterInterface $componnet, array $sku_list ){
-		parent::__construct($componnet);
-		if( count($sku_list) < 1 ){
-			throw new \Exception('[sku]错误');
-		}
-		
-		foreach( $sku_list as $it ){
-			$this->sku_info_list[] = [
-				'sku_id'	=> $it['sku_id'],
-				'num'		=> $it['num'],
-			];
-		}
-	}
 
     /**
      * 过滤
 	 * 计算 sku 基本数据
      * @return bool true：成功；false：失败
      */
-    public function filter(): bool
+    public function filter( array $params ): bool
     {
+        if( !parent::filter( $params ) ){
+            return false;
+        }
+        //
+
         return true;
     }
 
--- app/Order/Modules/Repository/Order/Creater/OrderCreaterInterface.php
+++ app/Order/Modules/Repository/Order/Creater/OrderCreaterInterface.php
@@ -17,16 +17,26 @@ interface OrderCreaterInterface
      * @return OrderCreater
      */
     public function getOrderCreater():OrderCreater;
-	
+
     /**
      * 过滤
      * <p>注意：</p>
      * <p>在过滤过程中，可以修改下单需要的元数据</p>
      * <p>组件之间的过滤操作互不影响</p>
      * <p>先执行内部组件的filter()，然后再执行组件本身的过滤</p>
+     * @param array $params     业务参数集合
+     * [
+     *      'user_id'   => ''，         //【必须】int 用户ID
+     *      'sku_list'  => [            //【必须】array sku列表参数，长度至少为1
+     *		    [
+     *			    'sku_id'	=> '',	//【必须】int SKU ID
+     *			    'num'		=> '',	//【必须】int 数量
+     *		    ]
+     *      ]
+     * ]
      * @return bool
      */
-    public function filter(): bool;
+    public function filter( array $params ): bool;
 
     /**
      * 获取数据结构
