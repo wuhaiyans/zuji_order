5d336480c2cd8437bc6963dd50f8d54cbc0ac8e0
zhangjinhui <zhangjinhui>
2018-06-08 11:59:12 +0800

    测试

--- app/Order/Modules/Service/OrderCreater.php
+++ app/Order/Modules/Service/OrderCreater.php
@@ -241,6 +241,7 @@ class OrderCreater
 
         } catch (\Exception $exc) {
             DB::rollBack();
+            print_r($exc);
             echo $exc->getMessage();
             die;
         }
