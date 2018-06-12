d87d6f1eaa3634bf325dbfe9d24a930265c76e2d
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-06-12 11:12:22 +0800

    修改状态流和操作日志

--- app/Lib/publicInc.php
+++ app/Lib/publicInc.php
@@ -18,4 +18,28 @@ class PublicInc
     const Type_Admin = 1; //管理员
     const Type_System = 3; // 系统自动化任务
     const Type_Store =4;//线下门店
+    /**
+     * 角色列表
+     * @return array
+     */
+    public static function getRoleList(){
+        return [
+            self::Type_User => '买家',
+            self::Type_Admin => '卖家',
+            self::Type_System => '系统',
+            self::Type_Store => '门店',
+        ];
+    }
+    /**
+     *  获取角色名称
+     * @param int $status
+     * @return string
+     */
+    public static function getRoleName($role){
+        $list = self::getRoleList();
+        if( isset($list[$role]) ){
+            return $list[$role];
+        }
+        return '';
+    }
 }
