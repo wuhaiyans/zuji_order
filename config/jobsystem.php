18e81bea451f14b82464a761e1ac499b1a7800b1
liuhongxing <1342935834@qq.com>
2018-07-05 15:59:19 +0800

    更新：任务系统实现

--- config/jobsystem.php
+++ config/jobsystem.php
@@ -3,6 +3,6 @@
 return [
 
 	'JOB_AUTH' => env('JOB_AUTH', '7ZT%SC8HB4*Ad$bWyEaj2mBy%qd2G49A'),
-	'JOB_API' => env('JOB_API','http://10.104.14.89:8082/javaserver/lts/jobs'),
-	'JOB_CALLBACK' => env('JOB_CALLBACK','http://dev-admin2-zuji.huishoubao.com/api/job/callback'),
+	'JOB_API' => env('JOB_API','http://jobserver:8082/javaserver/lts/jobs'),
+	'JOB_CALLBACK' => env('JOB_CALLBACK','http://admin.nqyong.com:1081/api/job/callback'),
 ];
