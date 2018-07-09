85e4afe27226f86f52152a1843051e874a51a270
limin <641260770@qq.com>
2018-07-09 11:31:46 +0800

    修改租期参数

--- app/Order/Modules/Service/OrderBuyout.php
+++ app/Order/Modules/Service/OrderBuyout.php
@@ -139,7 +139,7 @@ class OrderBuyout
 				'goods_name'=>'required',
 				'buyout_price'=>'required',
 				'zujin_price'=>'required',
-				'zujin_number'=>'required',
+				'zuqi_number'=>'required',
 				'amount'=>'required',
 				'create_time'=>'required',
 		]);
