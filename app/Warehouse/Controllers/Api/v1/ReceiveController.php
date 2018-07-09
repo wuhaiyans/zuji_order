2fa861bc86ded79d5c7237c91f0dd33a7ee6e1bf
wangjinlin <285253001@qq.com>
2018-07-09 15:26:52 +0800

    添加用户信息

--- app/Warehouse/Controllers/Api/v1/ReceiveController.php
+++ app/Warehouse/Controllers/Api/v1/ReceiveController.php
@@ -254,6 +254,7 @@ class ReceiveController extends Controller
         $param = request()->input();
         $userinfo=$param['userinfo'];
 
+        return \apiResponse($params, ApiStatus::CODE_10104, session()->get(self::SESSION_ERR_KEY));
         if (!$params) {
             return \apiResponse([], ApiStatus::CODE_10104, session()->get(self::SESSION_ERR_KEY));
         }
