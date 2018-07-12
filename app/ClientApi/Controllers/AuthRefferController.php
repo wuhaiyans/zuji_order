9733a46c56ecac349765e9a0463fe1f811706e9e
tiantang88 <466684860@qq.com>
2018-07-12 14:39:40 +0800

    转发限制

--- app/ClientApi/Controllers/AuthRefferController.php
+++ app/ClientApi/Controllers/AuthRefferController.php
@@ -30,47 +30,13 @@ class AuthRefferController extends Controller{
      */
     public function header(Request $request)
     {
-        //默认订单都需要验证，除了主动扣款
-        $params = $request->all();
-        $header = ['Content-Type: application/json'];
-        //是否需要验证
-        if(in_array($params['method'], config('clientAuth.exceptAuth'))){
-            $info = Curl::post(config('ordersystem.ORDER_API'), json_encode($params),$header);
-            Log::debug("验证token".$info);
-            $info =json_decode($info,true);
-            if( is_null($info)
-                || !is_array($info)
-                || !isset($info['code'])
-                || !isset($info['msg'])
-                || !isset($info['data']) ){
-                return response()->json([
-                    'code'  =>ApiStatus::CODE_20002,
-                    'msg'   => "稍候重试",
-                    'data'  =>$info
-                ]);
-            }
-            return response()->json($info);
-        }elseif(isset($params['auth_token']) && !in_array($params['method'], config('clientAuth.exceptAuth'))) {
-            $token  =   $params['auth_token'];
-            $checkInfo = User::checkToken($token);
-            Log::debug("验证token调用第三方User::checkToken返回的结果".print_r($checkInfo,true));
-            //验证不通过
-            if (is_null($checkInfo)
-                || !is_array($checkInfo)
-                || !isset($checkInfo['code'])
-                || !isset($checkInfo['msg'])
-                || !isset($checkInfo['data'])
-                || $checkInfo['code']!=0){
-                return response()->json($checkInfo);
-            }else{
+        try{
 
-                $params['userinfo']=[
-                    'uid'      =>$checkInfo['data'][0]['id'],
-                    'type'     =>2,       //用户类型（固定值1）：1：管理员；2：前端用户
-                    'username' =>$checkInfo['data'][0]['mobile']
-                ];
-                $list=['url'=>config('ordersystem.ORDER_API'),'data'=>$params];
-                Log::debug("验证token",$list);
+            //默认订单都需要验证，除了主动扣款
+            $params = $request->all();
+            $header = ['Content-Type: application/json'];
+            //是否需要验证
+            if(in_array($params['method'], config('clientAuth.exceptAuth'))){
                 $info = Curl::post(config('ordersystem.ORDER_API'), json_encode($params),$header);
                 Log::debug("验证token".$info);
                 $info =json_decode($info,true);
@@ -86,15 +52,62 @@ class AuthRefferController extends Controller{
                     ]);
                 }
                 return response()->json($info);
+            }elseif(isset($params['auth_token']) && !in_array($params['method'], config('clientAuth.exceptAuth'))) {
+                $token  =   $params['auth_token'];
+                $checkInfo = User::checkToken($token);
+                Log::debug("验证token调用第三方User::checkToken返回的结果".print_r($checkInfo,true));
+                //验证不通过
+                if (is_null($checkInfo)
+                    || !is_array($checkInfo)
+                    || !isset($checkInfo['code'])
+                    || !isset($checkInfo['msg'])
+                    || !isset($checkInfo['data'])
+                    || $checkInfo['code']!=0){
+                    return response()->json($checkInfo);
+                }else{
 
+                    $params['userinfo']=[
+                        'uid'      =>$checkInfo['data'][0]['id'],
+                        'type'     =>2,       //用户类型（固定值1）：1：管理员；2：前端用户
+                        'username' =>$checkInfo['data'][0]['mobile']
+                    ];
+                    $list=['url'=>config('ordersystem.ORDER_API'),'data'=>$params];
+                    Log::debug("验证token",$list);
+                    $info = Curl::post(config('ordersystem.ORDER_API'), json_encode($params),$header);
+                    Log::debug("验证token".$info);
+                    $info =json_decode($info,true);
+                    if( is_null($info)
+                        || !is_array($info)
+                        || !isset($info['code'])
+                        || !isset($info['msg'])
+                        || !isset($info['data']) ){
+                        return response()->json([
+                            'code'  =>ApiStatus::CODE_20002,
+                            'msg'   => "稍候重试",
+                            'data'  =>$info
+                        ]);
+                    }
+                    return response()->json($info);
+
+                }
+            }else{
+                return response()->json([
+                    'code'  =>ApiStatus::CODE_20003,
+                    'msg'   => "未登录",
+                    'data'  =>[''=>'']
+                ]);
             }
-        }else{
+
+        } catch (\Exception $e){
+
             return response()->json([
-                'code'  =>ApiStatus::CODE_20003,
-                'msg'   => "未登录",
+                'code'  => -1,
+                'msg'   => $e->getMessage(),
                 'data'  =>[''=>'']
             ]);
+
         }
+
     }
 
 
