105f67996b9aff8ff9417c7e61bf672cf57fc010
wangjinlin <285253001@qq.com>
2018-07-12 11:27:57 +0800

    修改确认收货

--- app/Lib/Warehouse/Receive.php
+++ app/Lib/Warehouse/Receive.php
@@ -256,7 +256,7 @@ class Receive
                 'goods_no' => $g->goods_no
             ];
             //退换货使用(支持多商品)
-            throw new \Exception( $g->refund_no);
+            throw new \Exception( json_encode($g));
             $refund_no[] = [
                 'refund_no'=>$g->refund_no
             ];
