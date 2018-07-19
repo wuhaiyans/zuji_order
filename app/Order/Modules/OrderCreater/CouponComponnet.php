c044f732ad9306db6ed46dabb76f2763b1814ae9
zhangjinhui <zhangjinhui>
2018-07-19 15:28:59 +0800

    测试

--- app/Order/Modules/OrderCreater/CouponComponnet.php
+++ app/Order/Modules/OrderCreater/CouponComponnet.php
@@ -37,17 +37,19 @@ class CouponComponnet implements OrderCreater
                     'coupon_no'=>$v,
                 ];
             }
-            var_dump($couponData);
+            print_r($couponData);
             $coupon = Coupon::getCoupon($couponData);
+            print_r($coupon);
             if(!is_array($coupon)){
                 throw new Exception("优惠券信息错误");
             }
             if(empty($coupon)){
                 throw new Exception("该优惠券已使用");
             }
+            print_r($coupon);
             $couponInfo =[];
             foreach ($coupon as $key=>$value){
-                var_dump($value);
+                print_r($value);die;
                 foreach ($value as $k=>$v){
                     $couponInfo[]=[
                         'coupon_id'=>$v['coupon_id'],
