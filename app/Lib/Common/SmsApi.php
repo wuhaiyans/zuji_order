85625c6043094a9d43c58c422edfb5f1b0571dc4
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-07-20 15:39:15 +0800

    短信调试

--- app/Lib/Common/SmsApi.php
+++ app/Lib/Common/SmsApi.php
@@ -101,6 +101,7 @@ class SmsApi {
 		];
 		$info = Curl::post($url, json_encode($data));
 		$info = json_decode($info,true);
+		LogApi::debug("短信调试",$info);
 		if ($info['code'] != 0) {
 			// 发短信失败
 			return false;
