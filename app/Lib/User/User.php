8eaf28c9666556bf630520786deebf44f083c6da
zhangjinhui <zhangjinhui>
2018-07-23 10:46:40 +0800

    测试

--- app/Lib/User/User.php
+++ app/Lib/User/User.php
@@ -130,6 +130,7 @@ class User{
         ];
         $info = Curl::post(config('tripartite.Interior_Goods_Url'), json_encode($data));
         $info =json_decode($info,true);
+        print_r($info);die;
         if(!is_array($info)){
             return ApiStatus::CODE_60000;
         }
