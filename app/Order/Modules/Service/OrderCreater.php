7d372db45ab9cf2e7c8c50ca85f30fe6a625534e
zhangjinhui <zhangjinhui>
2018-07-23 14:59:34 +0800

    测试

--- app/Order/Modules/Service/OrderCreater.php
+++ app/Order/Modules/Service/OrderCreater.php
@@ -197,10 +197,8 @@ class OrderCreater
             $schemaData = $orderCreater->getDataSchema();
             print_r($schemaData);
             $b = $orderCreater->create();
-            var_dump($b);
             //创建成功组装数据返回结果
             if(!$b){
-                echo 11;die;
                 DB::rollBack();
                 //把无法下单的原因放入到用户表中
                 User::setRemark($data['user_id'],$orderCreater->getOrderCreater()->getError());
@@ -219,13 +217,13 @@ class OrderCreater
             // 创建订单后 发送支付短信。;
             $orderNoticeObj = new OrderNotice(OrderStatus::BUSINESS_ZUJI,$data['order_no'],SceneConfig::ORDER_CREATE);
             $orderNoticeObj->notify();
-//            //发送取消订单队列（小程序取消订单队列）
-//            $b =JobQueueApi::addScheduleOnce(config('app.env')."OrderCancel_".$data['order_no'],config("ordersystem.ORDER_API"), [
-//                'method' => 'api.inner.miniCancelOrder',
-////                'order_no'=>$data['order_no'],
-////                'user_id'=>$data['user_id'],
-////                'time' => time(),
-//            ],time()+1800,"");
+            //发送取消订单队列（小程序取消订单队列）
+            $b =JobQueueApi::addScheduleOnce(config('app.env')."OrderCancel_".$data['order_no'],config("ordersystem.ORDER_API"), [
+                'method' => 'api.inner.miniCancelOrder',
+//                'order_no'=>$data['order_no'],
+//                'user_id'=>$data['user_id'],
+//                'time' => time(),
+            ],time()+1800,"");
             OrderLogRepository::add($data['user_id'],$schemaData['user']['user_mobile'],\App\Lib\PublicInc::Type_User,$data['order_no'],"下单","用户下单");
             return $result;
 
