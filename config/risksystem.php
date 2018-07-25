16874035936d27791dfbec2f5e0ac636a22248ad
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-07-25 10:19:52 +0800

    取消本地打印

--- config/risksystem.php
+++ config/risksystem.php
@@ -2,5 +2,5 @@
 // 风控系统配置
 return [
 //	'RISK_API' => env('RISK_API', 'http://risk.nqyong.com:1081/api'),
-	'RISK_API' => env('RISK_API', 'https://dev-api.nqyong.com/api/risk/api'),
+	'RISK_API' => env('RISK_API', 'http://fengkong.local.com/api'),
 ];
