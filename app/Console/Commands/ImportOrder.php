6edd4ce9e1e3d85e5b973bcfd9dee43fd3053250
tiantang88 <466684860@qq.com>
2018-07-26 10:33:33 +0800

    订单导入优化

--- app/Console/Commands/ImportOrder.php
+++ app/Console/Commands/ImportOrder.php
@@ -59,12 +59,16 @@ class ImportOrder extends Command
         $total = $this->conn->table('zuji_order2')->where(['business_key'=>1])->whereIn("appid",$appid)->count();
         $bar = $this->output->createProgressBar($total);
         try{
-            $limit = 5000;
+            $limit = 2000;
             $page =1;
             $totalpage = ceil($total/$limit);
             $arr =[];
+
             do {
-                $datas01 = $this->conn->table('zuji_order2')->where(['business_key'=>1])->whereIn("appid",$appid)->forPage($page,$limit)->get();
+                $whereArra = [];
+                $whereArra[] = ['business_key','=',1];
+                $whereArra[] = ['order_id','>=',($page-1)*$limit];
+                $datas01 = $this->conn->table('zuji_order2')->where($whereArra)->whereIn("appid",$appid)->orderBy('order_id','asc')->take($limit)->get();
                 $orders=objectToArray($datas01);
                 foreach ($orders as $k=>$v){
                     //获取渠道
@@ -228,7 +232,7 @@ class ImportOrder extends Command
                     $bar->advance();
                 }
                 $page++;
-                sleep(2);
+
             } while ($page <= $totalpage);
             $bar->finish();
             if(count($arr)>0){
