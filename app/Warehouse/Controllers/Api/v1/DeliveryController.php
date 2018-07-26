11b409feb8982b9dbdacdc2510a7feedc6ecec60
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-07-27 00:52:55 +0800

    发货请求测试

--- app/Warehouse/Controllers/Api/v1/DeliveryController.php
+++ app/Warehouse/Controllers/Api/v1/DeliveryController.php
@@ -297,6 +297,10 @@ class DeliveryController extends Controller
 
             //通知订单接口
             $a = \App\Lib\Warehouse\Delivery::delivery($orderDetail, $result['goods_info'], $user_info);
+            if(!$a){
+                DB::rollBack();
+                return \apiResponse([], ApiStatus::CODE_50000, "通知订单接口失败");
+            }
 
             //修改发货信息
             $this->delivery->send($params);
