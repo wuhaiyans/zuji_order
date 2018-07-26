8e3d0747ea5bdb556512a0f32ecb1650803eb9c4
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-07-26 11:13:06 +0800

    测试

--- app/Console/Commands/ImportOrder.php
+++ app/Console/Commands/ImportOrder.php
@@ -59,16 +59,20 @@ class ImportOrder extends Command
         $total = $this->conn->table('zuji_order2')->where(['business_key'=>1])->whereIn("appid",$appid)->count();
         $bar = $this->output->createProgressBar($total);
         try{
-            $limit = 5000;
+            $limit = 2000;
             $page =1;
             $totalpage = ceil($total/$limit);
             $arr =[];
 
             do {
+                sql_profiler();
                 $whereArra = [];
-//                $whereArra[] = ['business_key','=',1];
-//                $whereArra[] = ['order_id','>=',($page-1)*$limit+1];
-                $datas01 = $this->conn->table('zuji_order2')->where(['business_key'=>1])->whereIn("appid",$appid)->forPage($page,$limit)->get();
+                $whereArra[] = ['business_key','=',1];
+                $order =  $this->conn->table('zuji_order2')->orderBy('order_id','asc')->forPage($page)->take(1)->first();
+                $order=objectToArray($order);
+                $whereArra[] = ['order_id','>=',$order['order_id']];
+                $datas01 = $this->conn->table('zuji_order2')->where($whereArra)->whereIn("appid",$appid)->orderBy('order_id','asc')->take($limit)->get();
+                exit;
                 $orders=objectToArray($datas01);
                 foreach ($orders as $k=>$v){
                     //获取渠道
