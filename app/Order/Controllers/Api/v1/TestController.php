b8bc09b120cd067376882122dd0647ae234f6e56
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-08-03 17:03:51 +0800

    短信调试

--- app/Order/Controllers/Api/v1/TestController.php
+++ app/Order/Controllers/Api/v1/TestController.php
@@ -10,9 +10,10 @@ class TestController extends Controller
 
 
 		$dataSms = [
-			'code' =>'123456',
+			'realName' =>'马晓雨',
+			'buyoutPrice'	=> '123',
 		];
-		$a = \App\Lib\Common\SmsApi::sendMessage('13654565804', 'SMS_113461091  ', $dataSms);
+		$a = \App\Lib\Common\SmsApi::sendMessage('13654565804', 'SMS_113461145  ', $dataSms);
 		v($a);
 //		//发送短信
 //		$notice = new \App\Order\Modules\Service\OrderNotice(
