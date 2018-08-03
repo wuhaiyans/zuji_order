3e29af75d58a9af9447edf1663d62813baf96e95
zhangjinhui <zhangjinhui>
2018-08-03 20:39:40 +0800

    小程序默认取消订单

--- config/web.php
+++ config/web.php
@@ -22,6 +22,8 @@ return [
     'long_confirm_days' => 7*24*3600,
     //订单多长时间未支付取消订单
     'order_cancel_hours' =>2*3600,
+    //小程序订单多长时间未支付取消订单
+    'mini_order_cancel_hours' =>60,
     //买断单多长时间未支付取消买断支付单
     'buyout_cancel_hours' =>60*10,
     //检查androd审核情况
