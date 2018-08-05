061c74d29b4dffe0482a8478fe835f3ec2b6c42b
tiantang88 <466684860@qq.com>
2018-08-05 19:11:25 +0800

    sql监控执行sql语句

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +15,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if (config('app.debug')) {
+            Log::info('============ URL: ' . request()->fullUrl() . ' ===============');
+            DB::listen(function (QueryExecuted $query) {
+                $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+
+                $bindings = $query->connection->prepareBindings($query->bindings);
+                $pdo = $query->connection->getPdo();
+                $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+                $duration = $this->formatDuration($query->time / 1000);
+
+                Log::debug(sprintf('[%s] %s', $duration, $realSql));
+            });
+        }
+
         //
     }
 
@@ -24,5 +41,24 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+
+
+    /**
+     * Format duration.
+     *
+     * @param float $seconds
+     *
+     * @return string
+     */
+    private function formatDuration($seconds)
+    {
+        if ($seconds < 0.001) {
+            return round($seconds * 1000000) . 'μs';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000, 2) . 'ms';
+        }
+
+        return round($seconds, 2) . 's';
     }
 }
