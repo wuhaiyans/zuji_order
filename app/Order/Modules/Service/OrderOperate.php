d208827a073e11feb2407c23be2ee759fc024e33
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-08-06 13:36:12 +0800

    增加日志记录

--- app/Order/Modules/Service/OrderOperate.php
+++ app/Order/Modules/Service/OrderOperate.php
@@ -691,11 +691,13 @@ class OrderOperate
             $orderInfo['business_key'] = Inc\OrderStatus::BUSINESS_ZUJI;
             $orderInfo['business_no'] =$data['order_no'];
             $orderInfo['order_no']=$data['order_no'];
+            //通知收发货系统 -申请发货
             $delivery =Delivery::apply($orderInfo,$goodsInfo);
             if(!$delivery){
                 DB::rollBack();
                 return false;
             }
+            //增加操作日志
             $userInfo =$data['userinfo'];
             OrderLogRepository::add($userInfo['uid'],$userInfo['username'],\App\Lib\PublicInc::Type_Admin,$data['order_no'],"确认订单","后台申请发货");
 
@@ -896,13 +898,15 @@ class OrderOperate
         $goods = objectToArray($goods);
         $orderStatus =0;
         foreach ($goods as $k=>$v){
-            //查询是否有 未还机 未退款 未买断 订单就是未结束的 就返回
+            //判断订单状态是否是 退款/退货 如果是 状态为 退款关闭
             if($v['goods_status'] == Inc\OrderGoodStatus::REFUNDED || $v['goods_status'] == Inc\OrderGoodStatus::EXCHANGE_REFUND){
                 $orderStatus = Inc\OrderStatus::OrderClosedRefunded;
             }
+            //判断商品状态是 否是完成状态 买断/还机
             if($v['goods_status'] == Inc\OrderGoodStatus::COMPLETE_THE_MACHINE || $v['goods_status'] == Inc\OrderGoodStatus::BUY_OUT){
                 $orderStatus = Inc\OrderStatus::OrderCompleted;
             }
+            //查询是否有 未还机 未退款 未买断 订单就是未结束的 就返回
             if($v['goods_status']!=Inc\OrderGoodStatus::REFUNDED && $v['goods_status']!=Inc\OrderGoodStatus::COMPLETE_THE_MACHINE && $v['goods_status']!=Inc\OrderGoodStatus::BUY_OUT && $v['goods_status'] != Inc\OrderGoodStatus::EXCHANGE_REFUND){
             //var_dump("订单未完成");die;
                 return true;
