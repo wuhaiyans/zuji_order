6c0a09cc019928257b55647d4e9f5eee931c3573
zhangjinhui <zhangjinhui>
2018-08-06 11:28:54 +0800

    测试扣款

--- app/Lib/Payment/mini/MiniApi.php
+++ app/Lib/Payment/mini/MiniApi.php
@@ -42,11 +42,13 @@ class MiniApi {
             return false;
         }
         $result = $CommonMiniApi->getResult();
+        var_dump($result);
         \App\Lib\Common\LogApi::error('发送扣款请求',['request'=>$params,'response'=>$result]);
         //加redis订单扣款标示
         Redis::set('zuji:order:miniorder:orderno:'.$params['out_order_no'], 'MiniWithhold');
+        print_r($result);die;
         //返回字符串
-        return $result['zhima_merchant_order_credit_pay_response']['pay_status'];
+        return $result;
     }
 
     /*
