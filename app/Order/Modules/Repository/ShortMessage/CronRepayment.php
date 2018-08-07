f51c6bb29c75ef4f846207b4cccee36498a4ee92
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-08-07 14:44:08 +0800

    定时任务发送短信

--- app/Order/Modules/Repository/ShortMessage/CronRepayment.php
+++ app/Order/Modules/Repository/ShortMessage/CronRepayment.php
@@ -78,7 +78,6 @@ class CronRepayment implements ShortMessage {
             'zhifuLianjie'  => createShortUrl($zhifuLianjie),
             'serviceTel'    => config('tripartite.Customer_Service_Phone'),
         ];
-        p($dataSms,1);
         // 发送短息
         return \App\Lib\Common\SmsApi::sendMessage($userInfo['mobile'], $code, $dataSms);
 
