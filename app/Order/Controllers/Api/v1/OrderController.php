d119c99d24ab88474f9856542265d881f1790d30
tiantang88 <466684860@qq.com>
2018-08-07 19:26:58 +0800

    订单导出

--- app/Order/Controllers/Api/v1/OrderController.php
+++ app/Order/Controllers/Api/v1/OrderController.php
@@ -323,12 +323,13 @@ class OrderController extends Controller
      */
     public function orderListExport(Request $request) {
 
+
             $params = $request->all();
             if ((isset($params['size']) && $params['size']<500) || !isset($params['size']) ||  $params['size']>1000 ){
                  $params['size'] =  500;
             }
-            $params['size'] = $params['size'] && $params['size']<500  ?? 500;
-            $param['page'] = $params['page']?? 1;
+//            $params['size'] = $params['size'] && $params['size']<500  ?? 500;
+            $params['page'] = $params['page']?? 1;
             $orderData = Service\OrderOperate::getOrderExportList($params);
 
             if ($params['page']>20) {
