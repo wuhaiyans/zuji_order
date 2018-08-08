34e6911e9f8de9bad9d7e600afcc50d4a7029a80
tiantang88 <466684860@qq.com>
2018-08-08 13:24:50 +0800

    订单列表用户名输出

--- app/Order/Modules/Repository/OrderRepository.php
+++ app/Order/Modules/Repository/OrderRepository.php
@@ -747,7 +747,7 @@ class OrderRepository
 //           dd($orderIds);
 //            sql_profiler();
             $orderList =  DB::table('order_info as o')
-                ->select('o.order_no','o.order_amount','o.create_time','o.order_status','o.freeze_type','o.appid','o.pay_type','o.zuqi_type','o.user_id','o.mobile','d.address_info','v.visit_id','v.visit_text','v.id','l.logistics_no')
+                ->select('o.order_no','o.order_amount','o.create_time','o.order_status','o.freeze_type','o.appid','o.pay_type','o.zuqi_type','o.user_id','o.mobile','d.address_info','d.name','v.visit_id','v.visit_text','v.id','l.logistics_no')
                 ->whereIn('o.order_no', $orderIds)
                 ->join('order_user_address as d',function($join){
                     $join->on('o.order_no', '=', 'd.order_no');
