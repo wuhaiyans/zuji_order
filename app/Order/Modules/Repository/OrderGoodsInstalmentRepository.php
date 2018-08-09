0835ff99439bcde9dd057ad5d8452edc6932832c
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-08-09 23:29:22 +0800

    分期列表筛选 增加日期 day 筛选

--- app/Order/Modules/Repository/OrderGoodsInstalmentRepository.php
+++ app/Order/Modules/Repository/OrderGoodsInstalmentRepository.php
@@ -43,58 +43,64 @@ class OrderGoodsInstalmentRepository
      * 查询总数
      */
     public static function queryCount($param = []){
-        $whereArray = [];
 
-        // 开始时间（可选）
-        if( isset($param['begin_time']) && $param['begin_time'] != ""){
-            $whereArray[] =  ['term', '>=', $param['begin_time']];
-        }
+        // 结束时间 当前时间往后推两年
+        $endY = date('Y') + 2;
+        $endterm = $endY . date('md');
 
-        // 开始时间（可选）
-        if( isset($param['end_time']) && $param['end_time'] != ""){
-            $whereArray[] =  ['term', '<=', $param['end_time']];
-        }
+        // 开始时间默认为时间戳开始 时间19700101
+        $begin_time = !empty($param['begin_time']) ? $param['begin_time'] : '19700101';
+        $end_time   = !empty($param['end_time']) ? $param['end_time'] : $endterm;
+
+        $sql = "select count(*) as num from (SELECT CONCAT(term, day) AS termdate FROM order_goods_instalment LEFT JOIN order_info ON order_goods_instalment.order_no=order_info.order_no WHERE";
+        $where = " 1 = 1";
+
 
         //根据goods_no
         if (isset($param['goods_no']) && !empty($param['goods_no'])) {
-            $whereArray[] = ['order_goods_instalment.goods_no', '=', $param['goods_no']];
+            $where = $where . " AND order_goods_instalment.goods_no = '".$param['goods_no']."'";
         }
 
         //根据订单号
         if (isset($param['order_no']) && !empty($param['order_no'])) {
-            $whereArray[] = ['order_goods_instalment.order_no', '=', $param['order_no']];
+            $where = $where . " AND order_goods_instalment.order_no = '".$param['order_no']."'";
         }
 
         //根据分期状态
         if (isset($param['status']) && !empty($param['status'])) {
-            $whereArray[] = ['order_goods_instalment.status', '=', $param['status']];
+            $where = $where . " AND order_goods_instalment.status = '".$param['status']."'";
         }
 
         // 根据还款类型
         if (isset($param['pay_type']) && !empty($param['pay_type'])) {
-            $whereArray[] = ['order_goods_instalment.pay_type', '=', $param['pay_type']];
+            $where = $where . " AND order_goods_instalment.pay_type = '" . $param['pay_type']."'";
         }
 
         //根据分期日期
         if (isset($param['term']) && !empty($param['term'])) {
-            $whereArray[] = ['order_goods_instalment.term', '=', $param['term']];
+            $where = $where . " AND order_goods_instalment.term = '" . $param['term']."'";
         }
 
         //根据分期期数
         if (isset($param['times']) && !empty($param['times'])) {
-            $whereArray[] = ['order_goods_instalment.times', '=', $param['times']];
+            $where = $where . " AND order_goods_instalment.times = '" . $param['times']."'";
         }
 
         //根据用户手机号
         if (isset($param['mobile']) && !empty($param['mobile'])) {
-            $whereArray[] = ['order_info.mobile', '=', $param['mobile']];
+            $where = $where . " AND order_info.mobile = '" . $param['mobile']."'";
         }
 
 
-        $result = OrderGoodsInstalment::query()->where($whereArray)
-            ->leftJoin('order_info', 'order_info.order_no', '=', 'order_goods_instalment.order_no')
-            ->count();
-        return $result;//count($result);
+        $having = " HAVING termdate >= " . $begin_time . " AND termdate <= " . $end_time;
+        $sql = $sql . $where . $having . ') s';
+
+
+        $count = DB::select($sql);
+        $count = objectToArray($count);
+
+
+        return $count[0]['num'];
     }
     /**
      * 查询列表
@@ -106,15 +112,14 @@ class OrderGoodsInstalmentRepository
 
         $whereArray = [];
 
-        // 开始时间（可选）
-        if( isset($param['begin_time']) && $param['begin_time'] != ""){
-            $whereArray[] =  ['term', '>=', $param['begin_time']];
-        }
+        // 结束时间 当前时间往后推两年
+        $endY = date('Y') + 2;
+        $endterm = $endY . date('md');
 
-        // 开始时间（可选）
-        if( isset($param['end_time']) && $param['end_time'] != ""){
-            $whereArray[] =  ['term', '<=', $param['end_time']];
-        }
+        // 开始时间默认为时间戳开始 时间19700101
+        $begin_time = !empty($param['begin_time']) ? $param['begin_time'] : '19700101';
+        $end_time   = !empty($param['end_time']) ? $param['end_time'] : $endterm;
+
 
         //根据goods_no
         if (isset($param['goods_no']) && !empty($param['goods_no'])) {
@@ -146,11 +151,15 @@ class OrderGoodsInstalmentRepository
             $whereArray[] = ['order_info.mobile', '=', $param['mobile']];
         }
 
+
+        $whichs = "term,day";
         $result =  OrderGoodsInstalment::query()
-            ->select('order_goods_instalment.*','order_info.mobile','order_user_certified.realname')
+            ->select('order_goods_instalment.*','order_info.mobile','order_user_certified.realname',DB::raw('concat('.$whichs.') as termdate'))
             ->where($whereArray)
             ->leftJoin('order_info', 'order_info.order_no', '=', 'order_goods_instalment.order_no')
             ->leftJoin('order_user_certified', 'order_user_certified.order_no', '=', 'order_goods_instalment.order_no')
+            ->having('termdate','>=', $begin_time)
+            ->having('termdate','<=', $end_time)
             ->offset($offset)
             ->limit($pageSize)
             ->get();
