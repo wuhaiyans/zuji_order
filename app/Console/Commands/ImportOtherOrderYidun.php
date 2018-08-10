f8628a70317e0d91d56f5c467562a33ecf95af77
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-08-10 10:49:18 +0800

    修改

--- app/Console/Commands/ImportOtherOrderYidun.php
+++ app/Console/Commands/ImportOtherOrderYidun.php
@@ -46,7 +46,7 @@ class ImportOtherOrderYidun extends Command {
 			$totalpage = ceil($total / $limit);
 			$arr = [];
 			do {
-				$datas01 = \DB::connection('mysql_01')->table('zuji_order2_yidun')->leftJoin('zuji_order2', 'zuji_order2.order_id', '=', 'zuji_order2_yidun.order_id')->forPage($page, $limit)->get();
+				$datas01 = \DB::connection('mysql_01')->table('zuji_order2_yidun')->leftJoin('zuji_order2', 'zuji_order2.order_id', '=', 'zuji_order2_yidun.order_id')->where('zuji_order2.business_key', '<>', '1')->forPage($page, $limit)->get();
 				$yiduns = objectToArray($datas01);
 
 				foreach ($yiduns as $k => $v) {
