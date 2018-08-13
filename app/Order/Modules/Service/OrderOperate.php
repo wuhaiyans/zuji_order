120713f3b46847c26a722d9686c8fb4621250a40
tiantang88 <466684860@qq.com>
2018-08-14 01:15:38 +0800

    订单列表增加小程序订单号判断

--- app/Order/Modules/Service/OrderOperate.php
+++ app/Order/Modules/Service/OrderOperate.php
@@ -1079,7 +1079,12 @@ class OrderOperate
 
             $miniOrderData = OrderMiniRepository::getMiniOrderInfo($orderNo);
 
-            $orderData['zm_order_no']    =    $miniOrderData['zm_order_no'];
+            if ($miniOrderData) {
+
+                $orderData['zm_order_no']    =    $miniOrderData['zm_order_no']?? '';
+
+            }
+
 
         }
 
