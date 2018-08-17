53799407bfb0460f283bf6bf0821c9887f0c73e0
zhangjinhui <zhangjinhui>
2018-08-17 15:19:30 +0800

    测试还机流程

--- app/Order/Controllers/Api/v1/MiniNotifyController.php
+++ app/Order/Controllers/Api/v1/MiniNotifyController.php
@@ -379,14 +379,14 @@ class MiniNotifyController extends Controller
 //            'app_id'=>'2018032002411058',//小程序appid
 //        ]);
 
-//        $b = \App\Lib\Payment\mini\MiniApi::OrderClose([
-//            'out_order_no'=>'A817186122513088',//商户端订单号
-//            'zm_order_no'=>'2018081700001001097372503747',//芝麻订单号
-//            'out_trans_no'=>'A817186122513088',//商户端交易号
-//            'remark'=>'关闭订单操作',//订单操作说明
-//            'pay_amount'=>'0.00',//关闭金额
-//            'app_id'=>'2018032002411058',//小程序appid
-//        ]);
+        $b = \App\Lib\Payment\mini\MiniApi::OrderClose([
+            'out_order_no'=>'A815121608725347',//商户端订单号
+            'zm_order_no'=>'2018081500001001096947039038',//芝麻订单号
+            'out_trans_no'=>'A815121608725347',//商户端交易号
+            'remark'=>'关闭订单操作',//订单操作说明
+            'pay_amount'=>'0.00',//关闭金额
+            'app_id'=>'2018032002411058',//小程序appid
+        ]);
 
 //        $b = \App\Lib\Payment\mini\MiniApi::withhold([
 //            'out_order_no'=>'20180713000802',//商户端订单号
@@ -396,9 +396,9 @@ class MiniNotifyController extends Controller
 //            'pay_amount'=>'554.00',//关闭金额
 //            'app_id'=>'2018032002411058',//小程序appid
 //        ]);
-//        if($b == false){
-//            echo \App\Lib\Payment\mini\MiniApi::getError();die;
-//        }
+        if($b == false){
+            echo \App\Lib\Payment\mini\MiniApi::getError();die;
+        }
         echo '成功';die;
     }
 }
