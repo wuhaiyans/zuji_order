145bccd784c04f9018e0842c7e600ac76a6dfed8
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-08-23 11:29:44 +0800

    活动短信

--- app/Order/Controllers/Api/v1/ActiveController.php
+++ app/Order/Controllers/Api/v1/ActiveController.php
@@ -18,8 +18,7 @@ class ActiveController extends Controller
             $limit  = 5;
             $page   = 1;
             $sleep  = 20;
-//            $code   = "SMS_113461176";
-            $code   = "SMS_113461177";
+            $code   = "SMS_113461179";
 
 
             do {
@@ -35,36 +34,10 @@ class ActiveController extends Controller
 					break;
                 }
 
-                $webUrl = env('WEB_H5_URL');
-//                $url = isset($webUrl) ? $webUrl : 'https://h5.nqyong.com/';
-//                $url = $url  . 'https://h5.nqyong.com/myBillDetail?';
-				$url = 'https://h5.nqyong.com/myBillDetail?';
-
-
                 foreach($result as $item){
 
-//                    $orderInfo = \App\Order\Models\OrderGoods::where(['order_no'=>$item['order_no']])->first();
-//                    $orderInfo = objectToArray($orderInfo);
-
-                    $urlData = [
-                        'orderNo'       => $item['order_no'],     //  订单号
-                        'zuqi_type'     => $item['zuqi_type'],    //  租期类型
-                        'id'            => $item['instalment_id'],//  分期ID
-                        'appid'         => $item['appid'],        //  商品编号
-                        'goodsNo'       => $item['goods_no'],     //  商品编号
-                    ];
-
-                    $zhifuLianjie = $url . createLinkstringUrlencode($urlData);
-
                     $dataSms = [
-                        'realName'      => $item['realname'],
-                        'orderNo'       => $item['order_no'],
-                        'goodsName'     => $item['goods_name'],
-                        'zuJin'         => $item['amount'],
-                        'createTime'    => '2018-08-15',
-                        'zhifuLianjie'  => createShortUrl($zhifuLianjie),
-                        'serviceTel'    => config('tripartite.Customer_Service_Phone'),
-
+                        'newPhoneName'      => "iPhone X",
                     ];
 					// 发送短信
 					\App\Lib\Common\SmsApi::sendMessage($item['mobile'], $code, $dataSms);
@@ -77,11 +50,11 @@ class ActiveController extends Controller
             } while (true);
 
             if(count($arr) > 0){
-                \App\Lib\Common\LogApi::notify("提前还款短信", $arr);
+                \App\Lib\Common\LogApi::notify("活动运营短信", $arr);
             }
 
         }catch(\Exception $exc){
-            \App\Lib\Common\LogApi::debug('[活动短信发送失败]', ['msg'=>$exc->getMessage()]);
+            \App\Lib\Common\LogApi::debug('[活动运营短信]', ['msg'=>$exc->getMessage()]);
         }
     }
 
