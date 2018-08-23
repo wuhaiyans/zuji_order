b6d1df379532bfd9f46ca8c00b65bd154af4a632
limin <641260770@qq.com>
2018-08-23 15:45:16 +0800

    修改bug

--- app/Order/Modules/OrderExcel/CronOperator.php
+++ app/Order/Modules/OrderExcel/CronOperator.php
@@ -341,7 +341,7 @@ class CronOperator
             $item['order_type'] = Inc\OrderStatus::getTypeName($item['order_type']);
             $item['pay_type'] = Inc\PayInc::getPayName($item['pay_type']);
             $item['realname'] = empty($userList[$item['order_no']]['realname'])?"":$userList[$item['order_no']]['realname'];
-            $item['user_address'] = empty($userAddressList[$item['order_no']]['address_info'])?""$userAddressList[$item['order_no']]['address_info'];
+            $item['user_address'] = empty($userAddressList[$item['order_no']]['address_info'])?"":$userAddressList[$item['order_no']]['address_info'];
             $item['goods_name'] = $goodsList[$item['order_no']]['goods_name'];
             $item['zuqi'] = $goodsList[$item['order_no']]['zuqi'].Inc\OrderStatus::getZuqiTypeName($goodsList[$item['order_no']]['zuqi_type']);
             $item['zujin'] = $goodsList[$item['order_no']]['zujin'];
