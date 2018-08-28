a0560bfc81ea0ff4f00e34df666e4b4001e06869
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-08-28 14:33:49 +0800

    大疆无人机召回 短信

--- app/Order/Controllers/Api/v1/ActiveController.php
+++ app/Order/Controllers/Api/v1/ActiveController.php
@@ -18,7 +18,7 @@ class ActiveController extends Controller
             $limit  = 5;
             $page   = 1;
             $sleep  = 20;
-            $code   = "SMS_113461180";
+            $code   = "SMS_113461182";
 
 
             do {
@@ -37,8 +37,7 @@ class ActiveController extends Controller
                 foreach($result as $item){
 
                     $dataSms = [
-                        'newPhoneName'      => "iPhone X",
-                        'logisticsNo'       => "11111111111",
+                        'a'      => "",
                     ];
 					// 发送短信
 					\App\Lib\Common\SmsApi::sendMessage($item['mobile'], $code, $dataSms);
