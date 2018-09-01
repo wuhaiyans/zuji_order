18446e966ae3ca5863bb0025f053adfca138f28a
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-09-01 11:08:12 +0800

    保存用户身份类型

--- app/Order/Modules/OrderCreater/RiskComponnet.php
+++ app/Order/Modules/OrderCreater/RiskComponnet.php
@@ -118,23 +118,17 @@ class RiskComponnet implements OrderCreater
             }
         }
 
-        //保存是否是学生  是否通过学信网认证
-        $chsi = $this->knight['risk_detail']['chsi']?? true;
-        if(is_array($chsi) && !empty($chsi)){
-            $isChsi = $chsi['decision']==Risk::DecisionAccept?1:0; //判断是否通过学信网认证
-            $isStudent = $this->knight['is_chsi']?1:0;  //判断是否是学生
-
-            $certified = OrderUserCertified::where('order_no','=',$orderNo)->first();
-            if (!$certified) return false;
-            $certified->is_student = $isStudent;
-            $certified->is_chsi = $isChsi;
-            if (!$certified->save()) {
-                LogApi::error(config('app.env')."[下单]保存是否是学生和认证信息失败",$riskData);
-                $this->getOrderCreater()->setError('保存是否是学生和认证信息失败');
-                return false;
-            }
-
+        //保存是否是学生
+        $isStudent = $this->knight['is_chsi']?1:0;  //判断是否是学生
+        $certified = OrderUserCertified::where('order_no','=',$orderNo)->first();
+        if (!$certified) return false;
+        $certified->is_student = $isStudent;
+        if (!$certified->save()) {
+            LogApi::error(config('app.env')."[下单]保存用户身份类型失败",$riskData);
+            $this->getOrderCreater()->setError('保存用户身份类型失败');
+            return false;
         }
+
 
         return true;
 
