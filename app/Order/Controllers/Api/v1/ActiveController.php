10677dbb1a3600f506bca68dfee7217de9cf2142
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-09-20 10:42:31 +0800

    活动运行短信

--- app/Order/Controllers/Api/v1/ActiveController.php
+++ app/Order/Controllers/Api/v1/ActiveController.php
@@ -16,10 +16,10 @@ class ActiveController extends Controller
         ini_set('max_execution_time', '0');
         try{
             $arr =[];
-            $limit  = 50;
+            $limit  = 1;
             $page   = 1;
             $sleep  = 10;
-            $code   = "SMS_113461197";
+            $code   = "SMS_113461203";
 
 
             do {
@@ -37,34 +37,18 @@ class ActiveController extends Controller
 
                 foreach($result as $item){
 
-                    $url = 'https://h5.nqyong.com/myBillDetail?';
-
-                    $urlData = [
-                        'orderNo'       => $item['order_no'],     //  订单号
-                        'zuqi_type'     => $item['zuqi_type'],    //  租期类型
-                        'id'            => $item['id'],           //  分期ID
-                        'appid'         => $item['appid'],        //  商品编号
-                        'goodsNo'       => $item['goods_no'],     //  商品编号
-                    ];
-
-                    $zhifuLianjie   = $url . createLinkstringUrlencode($urlData);
                     $mobile         = trim($item['mobile']);
                     // 短信参数
                     $dataSms =[
-                        'realName'      => trim($item['realname']),
-                        'goodsName'     => trim($item['goods_name']),
-                        'zuJin'         => trim($item['amount']),
-                        'zhifuLianjie'  => createShortUrl($zhifuLianjie),
-                        'serviceTel'    => config('tripartite.Customer_Service_Phone'),
+                        'a'      => 'a',
                     ];
-
 					// 发送短信
 					\App\Lib\Common\SmsApi::sendMessage($mobile, $code, $dataSms);
 
                     \App\Order\Models\OrderActive::where(
                         ['id'=>$item['id']]
                     )->update(['status' => 1]);
-
+                die;
                 }
 
                 sleep($sleep);
