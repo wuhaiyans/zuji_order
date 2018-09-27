4aafe75ac4ca45506ed0f7d38401e5adb4c7f27a
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-09-27 16:52:21 +0800

    异常订单 工具操作控制器

---
+++ app/Order/Controllers/Api/v1/ToolController.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Order\Controllers\Api\v1;
+
+use Illuminate\Http\Request;
+
+
+
+class ToolController extends Controller
+{
+
+    /**
+     * 延期
+     * @author maxiaoyu
+     * @param Request $request
+     * $params[
+     *   'day'  => '', //【必须】int 延期天数
+     * ]
+     * @return bool true false
+     */
+    public function Delay(Request $request){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+?>
