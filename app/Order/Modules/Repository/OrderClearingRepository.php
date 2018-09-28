a07179e3e1452219701c4b7239e7ca821db497b4
tiantang88 <466684860@qq.com>
2018-09-28 16:14:03 +0800

    微回收清算回到接口

--- app/Order/Modules/Repository/OrderClearingRepository.php
+++ app/Order/Modules/Repository/OrderClearingRepository.php
@@ -322,6 +322,41 @@ class OrderClearingRepository
         return true;
     }
 
+
+
+
+    /**
+     * 更新乐百分结算完成状态
+     * Author: heaven
+     * @param $param
+     * @return bool
+     */
+    public static function upLebaiOrderCleanStatus($param){
+        if (empty($param)) {
+            return false;
+        }
+        $whereArray[] = ['payment_no', '=', $param['payment_no']];
+        $orderData =  OrderClearing::where($whereArray)->first();
+        if (!$orderData) return false;
+        if ($orderData->auth_unfreeze_status    ==  OrderCleaningStatus::depositUnfreezeStatusUnpayed) {
+            $orderData->auth_unfreeze_status  = OrderCleaningStatus::depositUnfreezeStatusPayd;
+            $orderData->auth_unfreeze_time  = time();
+            //判断预授权转支付是否为待支付状态，如果是，变更为已支付
+            if ($orderData->auth_deduction_status == OrderCleaningStatus::depositDeductionStatusUnpayed) {
+                $orderData->auth_deduction_status = OrderCleaningStatus::depositDeductionStatusPayd;
+                $orderData->auth_deduction_time = time();
+            }
+
+        }
+        $orderData->status  = OrderCleaningStatus::orderCleaningComplete;
+        $orderData->update_time = time();
+        $success =$orderData->save();
+        if(!$success){
+            return false;
+        }
+        return true;
+    }
+
     /**
      * 更新结算退款单数据
      * Author: heaven
