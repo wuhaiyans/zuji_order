7df9cc9166f8f18c7df42da4f2e44e3c6eb52e24
limin <641260770@qq.com>
2018-10-13 16:34:01 +0800

    保存

--- app/Activity/Controllers/Api/v1/ActiveInviteController.php
+++ app/Activity/Controllers/Api/v1/ActiveInviteController.php
@@ -38,6 +38,7 @@ class ActiveInviteController extends Controller
         }
         $invite_uid = $userInfo['uid'];
         $invite_mobile = $userInfo['username'];
+        $registerTime = $userInfo['register_time'];
         //解密邀请码获取用户id和活动id
         $codeNum = ExperienceDestineOperate::getInvitationCode($params['code']);
         $uid = $codeNum['user_id'];
@@ -52,9 +53,10 @@ class ActiveInviteController extends Controller
         if(!$checkStatus){
             return apiResponse([],ApiStatus::CODE_50000,"已邀请");
         }
+
         //验证是否新用户
-        if(date("Y-m-d") != date("Y-m-d",$userInfo['register_time'])){
-            return apiResponse($userInfo,ApiStatus::CODE_50001,"该用户不是新注册用户");
+        if(date("Y-m-d") != date("Y-m-d",$registerTime)){
+            return apiResponse($registerTime,ApiStatus::CODE_50001,"该用户不是新注册用户");
         }
 
         //更新邀请信息
