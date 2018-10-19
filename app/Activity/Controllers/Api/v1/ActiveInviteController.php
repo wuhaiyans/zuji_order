8e1f7d3e2edf66f847c6ba782c0fc4502519b5e5
limin <641260770@qq.com>
2018-10-19 18:57:37 +0800

    修改邀请人数限制

--- app/Activity/Controllers/Api/v1/ActiveInviteController.php
+++ app/Activity/Controllers/Api/v1/ActiveInviteController.php
@@ -61,7 +61,7 @@ class ActiveInviteController extends Controller
         //获预约活动信息
         $activityInfo = ExperienceDestineRepository::getUserExperience($uid,$activity_id);
         //检测邀请上限
-        if($activityInfo['zuqi']>=10){
+        if($activityInfo['zuqi']>=30){
             return apiResponse([],ApiStatus::CODE_50000,"已超过最大邀请人数");
         }
         //更新邀请信息
