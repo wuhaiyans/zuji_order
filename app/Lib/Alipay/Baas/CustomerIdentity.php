1c1d907d55e0bb5b4984436f53b2430cf0157d7e
liuhongxing <1342935834@qq.com>
2018-11-01 17:01:28 +0800

    新增：蚂蚁金服 金融科技 可信存证 API 封装

---
+++ app/Lib/Alipay/Baas/CustomerIdentity.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * 蚂蚁金服 金融科技 客户身份标识 封装
+ * @access public
+ * @author liuhongxing <liuhongxing@huishoubao.com.cn>
+ * @copyright (c) 2018, Huishoubao
+ */
+
+
+namespace App\Lib\Alipay\Bass;
+
+/**
+ * CustomerIdentity 客户身份标识 类
+ * @access public
+ * @author liuhongxing <liuhongxing@huishoubao.com.cn>
+ */
+class CustomerIdentity {
+	
+	private $userType = 'PERSON';
+	
+	private $certName = '';
+	private $certType = '';
+	private $certNo = '';
+	private $mobileNo = '';
+	private $properties = '';
+	
+	/**
+	 * 用户类型
+	 * @return string
+	 */
+	public function getUserType(): string{
+		return $this->userType;
+	}
+
+	/**
+	 * 设置 姓名（用户证件上的）
+	 * @param string $certName
+	 * @return \App\Lib\Alipay\Bass\CustomerIdentity
+	 */
+	public function setCertName(string $certName): CustomerIdentity {
+		$this->certName = $certName;
+		return $this;
+	}
+	/**
+	 * 读取 姓名（用户证件上的）
+	 * @return string
+	 */
+	public function getCertName(): string {
+		return $this->certName;
+	}
+
+
+	/**
+	 * 设置 证件类型
+	 * @param string $certType
+	 * @return \App\Lib\Alipay\Bass\CustomerIdentity
+	 */
+	public function setCertType(string $certType): CustomerIdentity {
+		$this->certType = $certType;
+		return $this;
+	}
+	/**
+	 * 读取 证件类型
+	 * @return string
+	 */
+	public function getCertType(): string {
+		return $this->certType;
+	}
+
+	/**
+	 * 设置 证件号
+	 * @param string $certNo
+	 * @return \App\Lib\Alipay\Bass\CustomerIdentity
+	 */
+	public function setCertNo(string $certNo): CustomerIdentity {
+		$this->certNo = $certNo;
+		return $this;
+	}
+	/**
+	 * 读取 证件号
+	 * @return string
+	 */
+	public function getCertNo(): string {
+		return $this->certNo;
+	}
+
+	/**
+	 * 设置 手机号
+	 * @param string $mobileNo
+	 * @return \App\Lib\Alipay\Bass\CustomerIdentity
+	 */
+	public function setMobileNo(string $mobileNo): CustomerIdentity {
+		$this->mobileNo = $mobileNo;
+		return $this;
+	}
+	/**
+	 * 读取 手机号
+	 * @return string
+	 */
+	public function getMobileNo(): string {
+		return $this->mobileNo;
+	}
+
+	/**
+	 * 设置 扩展参数
+	 * @param string $properties
+	 * @return \App\Lib\Alipay\Bass\CustomerIdentity
+	 */
+	public function setProperties(string $properties): CustomerIdentity {
+		$this->properties = $properties;
+		return $this;
+	}
+	/**
+	 * 读取 扩展参数
+	 * @return string
+	 */
+	public function getProperties(): string {
+		return $this->properties;
+	}
+
+
+}
