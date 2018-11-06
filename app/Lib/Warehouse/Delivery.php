8dabd3f204368201f42328e7aaef650a1330112c
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-11-06 19:14:42 +0800

    恢复调试

--- app/Lib/Warehouse/Delivery.php
+++ app/Lib/Warehouse/Delivery.php
@@ -340,7 +340,6 @@ class Delivery
 
       $response =\App\Lib\Order\Delivery::delivery($orderDetail, $goods_info,$operatorInfo);
       $response =json_decode($response,true);
-      return $response;
       if($response['code']!=ApiStatus::CODE_0){
           return false;
       }
