5ba1fda3dd2bf7d37759b2b71ded60db6744bd37
yaodongxu <yaodongxu@huishoubao.com.cn>
2018-11-06 11:53:18 +0800

    添加支付统一入口的还机业务

--- config/businesspay.php
+++ config/businesspay.php
@@ -9,5 +9,7 @@ return [
 	'business' => [
 	    // 业务类型为【买断】的工厂实例
 	    \App\Order\Modules\Inc\OrderStatus::BUSINESS_BUYOUT => App\Order\Modules\Repository\Buyout\Buyout::class,
+	    // 业务类型为【还机】的工厂实例
+	    \App\Order\Modules\Inc\OrderStatus::BUSINESS_GIVEBACK => App\Order\Modules\Repository\Giveback\GivebackPay::class,
 	],
 ];
