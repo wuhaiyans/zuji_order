f551461634f1ea1945759052f370c49852a25c28
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-11-06 11:53:54 +0800

    提前还款 支付组件

--- config/businesspay.php
+++ config/businesspay.php
@@ -9,5 +9,7 @@ return [
 	'business' => [
 	    // 业务类型为【买断】的工厂实例
 	    \App\Order\Modules\Inc\OrderStatus::BUSINESS_BUYOUT => App\Order\Modules\Repository\Buyout\Buyout::class,
+		// 业务类型为【分期主动支付】的工厂实例
+		\App\Order\Modules\Inc\OrderStatus::BUSINESS_FENQI => App\Order\Modules\Repository\Instalment\Instalment::class,
 	],
 ];
