6d3d682c174b516b8666f9445af2ef127c556e57
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-11-06 18:17:17 +0800

    修改发货日志

--- app/Warehouse/Controllers/Api/v1/DeliveryController.php
+++ app/Warehouse/Controllers/Api/v1/DeliveryController.php
@@ -333,7 +333,7 @@ class DeliveryController extends Controller
 
             //通知订单接口
             $a = \App\Lib\Warehouse\Delivery::delivery($orderDetail, $result['goods_info'], $user_info);
-            LogApi::info('delivery send order info :',$result['order_no'].$a);
+            LogApi::info('delivery send order info :',$orderDetail);
             if(!$a){
                 DB::rollBack();
                 return \apiResponse([], ApiStatus::CODE_50000, "通知订单接口失败");
