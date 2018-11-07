e841db5b08a6120b4fa4a0486fc7acd08b7fcdfb
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-11-07 18:25:07 +0800

    续租 线下支付 调试

--- app/Order/Modules/Repository/Pay/UnderPay/OrderRelet.php
+++ app/Order/Modules/Repository/Pay/UnderPay/OrderRelet.php
@@ -1,8 +1,8 @@
 <?php
 namespace App\Order\Modules\Repository\Pay\UnderPay;
 
-use App\Order\Modules\Repository\Instalment\Discounter\SimpleDiscounter;
-use App\Order\Modules\Repository\Instalment\Discounter\Discounter;
+use App\Lib\Common\LogApi;
+use App\Lib\Curl;
 
 class OrderRelet implements UnderLine {
 
@@ -28,21 +28,50 @@ class OrderRelet implements UnderLine {
      * return string
      */
     public function getPayAmount(){
-        $begin_time = $this->componnet['extend'][''];
-        $end_time = $this->componnet['extend'][''];
+
+        $order_no = $this->order_no;
+
+        $goods_obj = \App\Order\Modules\Repository\OrderGoodsRepository::getGoodsRow(['order_no'=>$order_no]);
+        $goodsInfo = objectToArray($goods_obj);
+        if(!$goodsInfo){
+            LogApi::debug('[underLinePay]获取商品信息错误：'.$this->order_no);
+            return false;
+        }
+        $spu_id = $goodsInfo['prod_id'];
+
+        $begin_time = $this->componnet['extend']['begin_time'];
+        $end_time   = $this->componnet['extend']['end_time'];
 
         $begin_time = strtotime($begin_time) > 0 ? strtotime($begin_time) : 0 ;
         $end_time   = strtotime($end_time) > 0 ? strtotime($end_time) : 0 ;
 
         if($begin_time >= $end_time){
-            return apiResponse([], ApiStatus::CODE_50000, "时间错误");
+            LogApi::debug('[underLinePay]续租时间错误：'.$this->order_no);
+            return false;
         }
-
 
         $end_time = $end_time + (3600 * 24) - 1;
         $day = ceil( ($end_time - $begin_time) / 86400 );
 
-       
+        /**
+         * 请求接口 计算商品总租金
+         */
+        $url = config('ordersystem.OLD_ORDER_API');
+        $data = [
+            'version'		=> '1.0',
+            'sign_type'		=> 'MD5',
+            'sign'			=> '',
+            'appid'			=> '1',
+            'method'		=> 'zuji.short.rent.price.get',
+            'timestamp' 	=> date("Y-m-d H:i:s"),
+            'params'		=> [
+                'zuqi'	    => $day,
+                'spu_id'	=> $spu_id,
+            ],
+        ];
+        $info = Curl::post($url, json_encode($data));
+        $info = json_decode($info,true);
+        p($info);
 
 
 
