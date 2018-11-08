6cd00c9cef3d353836afa1467ec8e88ef1dc4878
limin <641260770@qq.com>
2018-11-08 14:19:59 +0800

    保存

--- app/Activity/Controllers/Api/v1/AdvanceActivityController.php
+++ app/Activity/Controllers/Api/v1/AdvanceActivityController.php
@@ -156,7 +156,7 @@ class AdvanceActivityController extends Controller
         //获预约活动信息
         $activityInfo = ExperienceDestineRepository::getUserExperience($userInfo['uid'],1);
         if($activityInfo){
-            $activityInfo['zuqi_day'] = $activityInfo['zuqi'];
+            $activityInfo['zuqi_day'] = $count;
             $activityInfo['zuqi'] -= $count;
             $activityInfo['type'] = 2;
             $activityInfo['content'] = '尊敬的客户您好，请您于2018年11月25日10：00点——19:00到店领取商品。 地址为：天津市西青区师范大学南门华木里底商711便利店直走100米——拿趣用数码共享便利店。 客服电话：18611002204';
