b0e91991e8e6f19f062e9ada22e5a2d67697ffd6
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-11-10 15:07:28 +0800

    调试风控保存

--- app/Order/Modules/Service/OrderOperate.php
+++ app/Order/Modules/Service/OrderOperate.php
@@ -756,11 +756,11 @@ class OrderOperate
             return  ApiStatus::CODE_31006;
         }
         //开启事务
-        DB::beginTransaction();
+
         //查询订单信息
         $order = $order = Order::getByNo($orderNo);
         if(!$order){
-            DB::rollBack();
+   
             LogApi::error(config('app.env')."[orderRiskSave] Order-non-existent:".$orderNo);
             return ApiStatus::CODE_31006;
         }
@@ -772,7 +772,7 @@ class OrderOperate
         }
         $b = $order->editOrderRiskStatus($riskStatus);
         if(!$b){
-            DB::rollBack();
+
             LogApi::error(config('app.env')."[orderRiskSave] Order-editOrderRiskStatus:".$orderNo);
             return ApiStatus::CODE_31006;
         }
@@ -780,7 +780,7 @@ class OrderOperate
         //保存风控审核日志
         $b =OrderRiskCheckLogRepository::add(0,"系统",\App\Lib\PublicInc::Type_System,$orderNo,"系统风控操作",$riskStatus);
         if(!$b){
-            DB::rollBack();
+
             LogApi::error(config('app.env')."[orderRiskSave] save-orderRiskCheckLogErro:".$orderNo);
             return ApiStatus::CODE_31006;
         }
@@ -805,7 +805,6 @@ class OrderOperate
             LogApi::info(config('app.env')."[orderRiskSave]save-success：",$riskData);
             return  ApiStatus::CODE_0;
         }
-        DB::commit();
 
 
     }
