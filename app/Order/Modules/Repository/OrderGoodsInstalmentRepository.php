1ce2ca665c03bc44351b2bf51f5614d08b548aa2
zhengtianhe <zhengtianhe@huishoubao.com>
2018-11-19 11:23:04 +0800

    逾期查询

--- app/Order/Modules/Repository/OrderGoodsInstalmentRepository.php
+++ app/Order/Modules/Repository/OrderGoodsInstalmentRepository.php
@@ -133,7 +133,10 @@ class OrderGoodsInstalmentRepository
             $whereArray[] = ['order_goods_instalment.goods_no', '=', $param['goods_no']];
         }
 
-
+        //逾期天数
+        if(isset($param['beoverdue_day']) && !empty($param['beoverdue_day'])){
+            $whereArray = getBeoverduetime($param['beoverdue_day']);
+        }
         //根据订单号
         if (isset($param['order_no']) && !empty($param['order_no'])) {
             $whereArray[] = ['order_goods_instalment.order_no', '=', $param['order_no']];
