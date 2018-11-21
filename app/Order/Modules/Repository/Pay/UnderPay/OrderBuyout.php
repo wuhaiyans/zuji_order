d92e6fc9c1b7a20218ce90363df25d9d46d41707
limin <641260770@qq.com>
2018-11-21 14:48:40 +0800

    线下买断冻结订单

--- app/Order/Modules/Repository/Pay/UnderPay/OrderBuyout.php
+++ app/Order/Modules/Repository/Pay/UnderPay/OrderBuyout.php
@@ -235,10 +235,18 @@ class OrderBuyout implements UnderLine {
             ];
             self::log($orderLog,$goodsLog);
         }
+        
+        $ret = OrderRepository::orderFreezeUpdate($buyout['order_no'],OrderFreezeStatus::Buyout);
+        if(!$ret){
+            LogApi::info("offline-buyout","线下买断冻结订单失败");
+        }
+
         if(isset($buyout['id'])){
+            //更新买断单
             $ret = OrderBuyoutRepository::setOrderRelease($buyout['id']);
         }
         else{
+            //创建买断单
             $ret = OrderBuyoutRepository::create($buyout);
         }
 
