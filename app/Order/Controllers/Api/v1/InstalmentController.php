19afb7a3a2e038ca12e8fed26f69eae6ac4fbb6b
maxiaoyu <maxiaoyu@huishoubao.com.cn>
2018-11-27 15:47:45 +0800

    分期列表 逾期筛选

--- app/Order/Controllers/Api/v1/InstalmentController.php
+++ app/Order/Controllers/Api/v1/InstalmentController.php
@@ -58,7 +58,6 @@ class InstalmentController extends Controller
             }
         }        
         $params['is_instalment_list'] = 1;
-        sql_profiler();
         $list = \App\Order\Modules\Repository\OrderGoodsInstalmentRepository::queryList($params,$additional);   
         foreach($list as &$item){
 
