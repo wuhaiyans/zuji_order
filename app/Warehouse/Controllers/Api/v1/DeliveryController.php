4464e0a321148ae20e9594c6b5d0adc43c87a3ef
wangjinlin <285253001@qq.com>
2018-12-20 18:34:55 +0800

    test

--- app/Warehouse/Controllers/Api/v1/DeliveryController.php
+++ app/Warehouse/Controllers/Api/v1/DeliveryController.php
@@ -521,7 +521,6 @@ class DeliveryController extends Controller
         $request = request()->input();
         $params['channel_id'] = json_decode($request['userinfo']['channel_id'], true);
         $list = $this->delivery->lists($params);
-        WarehouseWarning::warningWarehouse('[订单发货]失败',[$list]);
         return \apiResponse($list);
     }
 
