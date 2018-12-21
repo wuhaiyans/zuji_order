c2039200633cd4469f07b9a51112c30cca2c491b
wangjinlin <285253001@qq.com>
2018-12-21 18:17:00 +0800

    联调2

--- app/Warehouse/Controllers/Api/v1/ReceiveController.php
+++ app/Warehouse/Controllers/Api/v1/ReceiveController.php
@@ -324,7 +324,7 @@ class ReceiveController extends Controller
             return \apiResponse([], ApiStatus::CODE_10104, session()->get(self::SESSION_ERR_KEY));
         }
 
-        return apiResponse($params = request()->input());
+        return apiResponse($_FILES['params']);
         $update_obj = new TencentUpload();
         $upload_imgs = $update_obj->file_upload_all();
         LogApi::info('checkItemsFinish_info_Receive2',$upload_imgs);
