79ef6a4772faa5eea6b3b6d8b8be64139db7187d
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2018-12-24 14:15:39 +0800

    增加逾期扣款文件

---
+++ app/Order/Controllers/Api/v1/OverDueDeductionController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Order\Controllers\Api\v1;
+use App\Lib\Common\LogApi;
+class OverDueDeductionController extends Controller
+{
+    /**
+     * 逾期扣款列表
+     */
+    public function overdueDeductionList(){
+
+    }
+
+}
