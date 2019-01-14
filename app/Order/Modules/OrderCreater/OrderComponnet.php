37c58969ba80f1908192d4a6a5b6cc4ea34ec5d1
wuhaiyans@aliyun.com <wuhaiyan@aliyun.com>
2019-01-14 18:17:11 +0800

    修改判断

--- app/Order/Modules/OrderCreater/OrderComponnet.php
+++ app/Order/Modules/OrderCreater/OrderComponnet.php
@@ -185,7 +185,8 @@ class OrderComponnet implements OrderCreater
        // $res =Redis::set("OrderWhiteList",json_encode([$mobile]));
         $res = Redis::get("OrderWhiteList");
         $whiteList = json_decode($res,true);
-        if(is_array($whiteList) && !in_array($mobile,$whiteList) && !empty($res)){
+
+        if(empty($res) || (is_array($whiteList) && !in_array($mobile,$whiteList))){
             $b =OrderRepository::unCompledOrder($this->userId);
             if($b) {
                 set_code(ApiStatus::CODE_30006);
