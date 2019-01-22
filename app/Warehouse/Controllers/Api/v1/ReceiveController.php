c4267d5e134340ae6a60b774eed7009adfbbec4f
wangjinlin <285253001@qq.com>
2019-01-22 17:28:05 +0800

    检测通知区块链

--- app/Warehouse/Controllers/Api/v1/ReceiveController.php
+++ app/Warehouse/Controllers/Api/v1/ReceiveController.php
@@ -348,7 +348,7 @@ class ReceiveController extends Controller
             LogApi::info('checkItemsFinish_info_Receive',$items);
 
             $this->receive->checkItem($params);
-            Receive::checkItemsResult($items,$receive_row['business_key'],$userinfo);
+            //Receive::checkItemsResult($items,$receive_row['business_key'],$userinfo);
 
             DB::commit();
 
