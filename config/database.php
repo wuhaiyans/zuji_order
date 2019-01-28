efe96c2ddd9117f1c2d35afbc0fc402d95adb003
gaobo <414052702@qq.com>
2019-01-28 18:29:08 +0800

    增加营销工具配置

--- config/database.php
+++ config/database.php
@@ -155,6 +155,21 @@ return [
 			'strict' => true,
 			'engine' => null,
 		],
+        
+        'tool' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TOOL_HOST', '127.0.0.1'),
+            'port' => env('DB_TOOL_PORT', '3306'),
+            'database' => env('DB_TOOL_DATABASE', 'zuji_tool'),
+            'username' => env('DB_TOOL_USERNAME', 'root'),
+            'password' => env('DB_TOOL_PASSWORD', 'root'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
 
         'orderuser' => [
             'driver' => 'mysql',
