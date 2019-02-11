8634e16dc0d220159862b152122f37b03182a9fc
tiantang88 <466684860@qq.com>
2019-02-11 19:26:27 +0800

    取消订单问题修复

--- app/Order/Modules/Repository/OrderRepository.php
+++ app/Order/Modules/Repository/OrderRepository.php
@@ -768,9 +768,9 @@ class OrderRepository
                 $whereArray[] = ['order_info.create_time', '>', 0];
                 $count = DB::table('order_info')
                     ->select(DB::raw('count(order_info.order_no) as order_count'))
-                    ->join('order_user_address',function($join){
-                        $join->on('order_info.order_no', '=', 'order_user_address.order_no');
-                    }, null,null,'inner')
+//                    ->join('order_user_address',function($join){
+//                        $join->on('order_info.order_no', '=', 'order_user_address.order_no');
+//                    }, null,null,'inner')
                     ->first();
 
             } else {
